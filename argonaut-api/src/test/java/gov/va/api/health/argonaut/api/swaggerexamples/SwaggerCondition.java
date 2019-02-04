755c78c41c7adf1cfd49fcb76f2e1b7b21d49bc5
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-02-04 10:35:19 -0500

    Proof of concept for injection of builder-based Swagger examples.

---
+++ argonaut-api/src/test/java/gov/va/api/health/argonaut/api/swaggerexamples/SwaggerCondition.java
@@ -0,0 +1,64 @@
+package gov.va.api.health.argonaut.api.swaggerexamples;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+class SwaggerCondition {
+  //    SwaggerExamples.CONDITION
+
+  //    SwaggerExamples.CONDITION_BUNDLE
+
+  //    SwaggerExamples.DIAGNOSTIC_REPORT
+
+  //    SwaggerExamples.DIAGNOSTIC_REPORT_BUNDLE
+
+  //    SwaggerExamples.ENCOUNTER
+
+  //    SwaggerExamples.ENCOUNTER_BUNDLE
+
+  //    SwaggerExamples.IMMUNIZATION
+
+  //    SwaggerExamples.IMMUNIZATION_BUNDLE
+
+  //    SwaggerExamples.LOCATION
+
+  //    SwaggerExamples.LOCATION_BUNDLE
+
+  //    SwaggerExamples.MEDICATION
+
+  //    SwaggerExamples.MEDICATION_BUNDLE
+
+  //    SwaggerExamples.MEDICATION_DISPENSE
+
+  //    SwaggerExamples.MEDICATION_DISPENSE_BUNDLE
+
+  //    SwaggerExamples.MEDICATION_ORDER
+
+  //    SwaggerExamples.MEDICATION_ORDER_BUNDLE
+
+  //    SwaggerExamples.MEDICATION_STATEMENT
+
+  //    SwaggerExamples.MEDICATION_STATEMENT_BUNDLE
+
+  //    SwaggerExamples.OBSERVATION
+
+  //    SwaggerExamples.OBSERVATION_BUNDLE
+
+  //    SwaggerExamples.OPERATION_OUTCOME
+
+  //    SwaggerExamples.ORGANZIATION_BUNDLE
+
+  //    SwaggerExamples.ORGANZIATON
+
+  //    SwaggerExamples.PATIENT
+
+  //    SwaggerExamples.PATIENT_BUNDLE
+
+  //    SwaggerExamples.PRACTITIONER
+
+  //    SwaggerExamples.PRACTITIONER_BUNDLE
+
+  //    SwaggerExamples.PROCEDURE
+
+  //    SwaggerExamples.PROCEDURE_BUNDLE
+}
