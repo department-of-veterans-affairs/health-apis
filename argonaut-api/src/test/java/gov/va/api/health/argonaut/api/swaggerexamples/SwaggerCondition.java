7fcbc773511f3e658cefa82118f9ba8ec4295b9d
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-02-04 12:56:41 -0500

    Condition Swagger examples.

--- argonaut-api/src/test/java/gov/va/api/health/argonaut/api/swaggerexamples/SwaggerCondition.java
+++ argonaut-api/src/test/java/gov/va/api/health/argonaut/api/swaggerexamples/SwaggerCondition.java
@@ -1,24 +1,34 @@
 package gov.va.api.health.argonaut.api.swaggerexamples;
 
+import static java.util.Arrays.asList;
+
+import gov.va.api.health.argonaut.api.bundle.AbstractBundle.BundleType;
+import gov.va.api.health.argonaut.api.bundle.AbstractEntry.Search;
+import gov.va.api.health.argonaut.api.bundle.AbstractEntry.SearchMode;
+import gov.va.api.health.argonaut.api.bundle.BundleLink;
+import gov.va.api.health.argonaut.api.bundle.BundleLink.LinkRelation;
+import gov.va.api.health.argonaut.api.datatypes.CodeableConcept;
+import gov.va.api.health.argonaut.api.datatypes.Coding;
+import gov.va.api.health.argonaut.api.elements.Reference;
 import gov.va.api.health.argonaut.api.resources.Condition;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 class SwaggerCondition {
   static final Condition SWAGGER_EXAMPLE_CONDITION =
-      XXX.builder()
+      Condition.builder()
           .resourceType("Condition")
           .id("b34bacd3-42b6-5613-b1c2-1abafe1248ba")
           .patient(
-              XXX.builder()
+              Reference.builder()
                   .reference("https://dev-api.va.gov/services/argonaut/v0/Patient/2000163")
                   .display("Mr. Aurelio227 Cruickshank494")
                   .build())
           .code(
-              XXX.builder()
+              CodeableConcept.builder()
                   .coding(
                       asList(
-                          XXX.builder()
+                          Coding.builder()
                               .code("38341003")
                               .system("https://www.snomed.org/snomed-ct")
                               .display("Hypertension")
@@ -26,180 +36,83 @@ class SwaggerCondition {
                   .text("Hypertension")
                   .build())
           .category(
-              XXX.builder()
+              CodeableConcept.builder()
                   .coding(
                       asList(
-                          XXX.builder().system("http://argonaut.hl7.org").code("problem").build()))
+                          Coding.builder()
+                              .system("http://argonaut.hl7.org")
+                              .code("problem")
+                              .build()))
                   .build())
-          .clinicalStatus("active")
-          .verificationStatus("unknown")
+          .clinicalStatus(Condition.ClinicalStatusCode.active)
+          .verificationStatus(Condition.VerificationStatusCode.unknown)
           .dateRecorded("2013-04-14")
           .onsetDateTime("2013-04-15T01:15:52Z")
           .build();
 
-  //	 static final String CONDITION =
-  //		      "{ "
-  //		          + "    \"resourceType\": \"Condition\", "
-  //		          + "    \"id\": \"b34bacd3-42b6-5613-b1c2-1abafe1248ba\", "
-  //		          + "    \"patient\": { "
-  //		          + "        \"reference\": \"https://dev-api.va.gov/services/argonaut/v0/Patient/2000163\", "
-  //		          + "        \"display\": \"Mr. Aurelio227 Cruickshank494\" "
-  //		          + "    }, "
-  //		          + "    \"code\": { "
-  //		          + "        \"coding\": [ "
-  //		          + "            { "
-  //		          + "                \"code\": \"38341003\", "
-  //		          + "                \"system\": \"https://www.snomed.org/snomed-ct\", "
-  //		          + "                \"display\": \"Hypertension\" "
-  //		          + "            } "
-  //		          + "        ], "
-  //		          + "        \"text\": \"Hypertension\" "
-  //		          + "    }, "
-  //		          + "    \"category\": { "
-  //		          + "        \"coding\": [ "
-  //		          + "            { "
-  //		          + "                \"system\": \"http://argonaut.hl7.org\", "
-  //		          + "                \"code\": \"problem\" "
-  //		          + "            } "
-  //		          + "        ] "
-  //		          + "    }, "
-  //		          + "    \"clinicalStatus\": \"active\", "
-  //		          + "    \"verificationStatus\": \"unknown\", "
-  //		          + "    \"dateRecorded\": \"2013-04-14\", "
-  //		          + "    \"onsetDateTime\": \"2013-04-15T01:15:52Z\" "
-  //		          + "} ";
-
   static final Condition.Bundle SWAGGER_EXAMPLE_CONDITION_BUNDLE =
-      XXX.builder()
+      Condition.Bundle.builder()
           .resourceType("Bundle")
-          .type("searchset")
+          .type(BundleType.searchset)
           .total(1)
           .link(
               asList(
-                  XXX.builder()
-                      .relation("self")
+                  BundleLink.builder()
+                      .relation(LinkRelation.self)
                       .url(
                           "https://dev-api.va.gov/services/argonaut/v0/Condition?patient=1017283148V813263&page=1&_count=15")
                       .build(),
-                  XXX.builder()
-                      .relation("first")
+                  BundleLink.builder()
+                      .relation(LinkRelation.first)
                       .url(
                           "https://dev-api.va.gov/services/argonaut/v0/Condition?patient=1017283148V813263&page=1&_count=15")
                       .build(),
-                  XXX.builder()
-                      .relation("last")
+                  BundleLink.builder()
+                      .relation(LinkRelation.last)
                       .url(
                           "https://dev-api.va.gov/services/argonaut/v0/Condition?patient=1017283148V813263&page=1&_count=15")
                       .build()))
           .entry(
               asList(
-                  XXX.builder()
+                  Condition.Entry.builder()
                       .fullUrl(
                           "https://dev-api.va.gov/services/argonaut/v0/Condition/b34bacd3-42b6-5613-b1c2-1abafe1248ba")
                       .resource(
-                          XXX.builder()
-                              .fullUrl(
-                                  "https://dev-api.va.gov/services/argonaut/v0/Condition/b34bacd3-42b6-5613-b1c2-1abafe1248ba")
-                              .resource(
-                                  XXX.builder()
-                                      .resourceType("Condition")
-                                      .id("b34bacd3-42b6-5613-b1c2-1abafe1248ba")
-                                      .patient(
-                                          XXX.builder()
-                                              .reference(
-                                                  "https://dev-api.va.gov/services/argonaut/v0/Patient/2000163")
-                                              .display("Mr. Aurelio227 Cruickshank494")
-                                              .build())
-                                      .code(
-                                          XXX.builder()
-                                              .coding(
-                                                  asList(
-                                                      XXX.builder()
-                                                          .code("38341003")
-                                                          .system(
-                                                              "https://www.snomed.org/snomed-ct")
-                                                          .display("Hypertension")
-                                                          .build()))
-                                              .text("Hypertension")
-                                              .build())
-                                      .category(
-                                          XXX.builder()
-                                              .coding(
-                                                  asList(
-                                                      XXX.builder()
-                                                          .system("http://argonaut.hl7.org")
-                                                          .code("problem")
-                                                          .build()))
-                                              .build())
-                                      .clinicalStatus("active")
-                                      .verificationStatus("unknown")
-                                      .dateRecorded("2013-04-14")
-                                      .onsetDateTime("2013-04-15T01:15:52Z")
+                          Condition.builder()
+                              .resourceType("Condition")
+                              .id("b34bacd3-42b6-5613-b1c2-1abafe1248ba")
+                              .patient(
+                                  Reference.builder()
+                                      .reference(
+                                          "https://dev-api.va.gov/services/argonaut/v0/Patient/2000163")
+                                      .display("Mr. Aurelio227 Cruickshank494")
                                       .build())
-                              .search(XXX.builder().mode("match").build())
+                              .code(
+                                  CodeableConcept.builder()
+                                      .coding(
+                                          asList(
+                                              Coding.builder()
+                                                  .code("38341003")
+                                                  .system("https://www.snomed.org/snomed-ct")
+                                                  .display("Hypertension")
+                                                  .build()))
+                                      .text("Hypertension")
+                                      .build())
+                              .category(
+                                  CodeableConcept.builder()
+                                      .coding(
+                                          asList(
+                                              Coding.builder()
+                                                  .system("http://argonaut.hl7.org")
+                                                  .code("problem")
+                                                  .build()))
+                                      .build())
+                              .clinicalStatus(Condition.ClinicalStatusCode.active)
+                              .verificationStatus(Condition.VerificationStatusCode.unknown)
+                              .dateRecorded("2013-04-14")
+                              .onsetDateTime("2013-04-15T01:15:52Z")
                               .build())
+                      .search(Search.builder().mode(SearchMode.match).build())
                       .build()))
           .build();
-  
-//  static final String CONDITION_BUNDLE =
-//	      "{ "
-//	          + "    \"resourceType\": \"Bundle\", "
-//	          + "    \"type\": \"searchset\", "
-//	          + "    \"total\": 1, "
-//	          + "    \"link\": [ "
-//	          + "        { "
-//	          + "            \"relation\": \"self\", "
-//	          + "            \"url\": \"https://dev-api.va.gov/services/argonaut/v0/Condition?patient=1017283148V813263&page=1&_count=15\" "
-//	          + "        }, "
-//	          + "        { "
-//	          + "            \"relation\": \"first\", "
-//	          + "            \"url\": \"https://dev-api.va.gov/services/argonaut/v0/Condition?patient=1017283148V813263&page=1&_count=15\" "
-//	          + "        }, "
-//	          + "        { "
-//	          + "            \"relation\":\"last\", "
-//	          + "            \"url\": \"https://dev-api.va.gov/services/argonaut/v0/Condition?patient=1017283148V813263&page=1&_count=15\" "
-//	          + "        } "
-//	          + "    ], "
-//	          + "    \"entry\": [ "
-//	          + "        { "
-//	          + "            \"fullUrl\": \"https://dev-api.va.gov/services/argonaut/v0/Condition/b34bacd3-42b6-5613-b1c2-1abafe1248ba\", "
-//	          + "            \"resource\": { "
-//	          + "                \"fullUrl\": \"https://dev-api.va.gov/services/argonaut/v0/Condition/b34bacd3-42b6-5613-b1c2-1abafe1248ba\", "
-//	          + "                \"resource\": { "
-//	          + "                    \"resourceType\": \"Condition\", "
-//	          + "                    \"id\": \"b34bacd3-42b6-5613-b1c2-1abafe1248ba\", "
-//	          + "                    \"patient\": { "
-//	          + "                        \"reference\": \"https://dev-api.va.gov/services/argonaut/v0/Patient/2000163\", "
-//	          + "                        \"display\": \"Mr. Aurelio227 Cruickshank494\" "
-//	          + "                    }, "
-//	          + "                    \"code\": { "
-//	          + "                        \"coding\": [ "
-//	          + "                           { "
-//	          + "                               \"code\": \"38341003\", "
-//	          + "                               \"system\": \"https://www.snomed.org/snomed-ct\", "
-//	          + "                               \"display\": \"Hypertension\" "
-//	          + "                           } "
-//	          + "                        ], "
-//	          + "                        \"text\": \"Hypertension\" "
-//	          + "                    }, "
-//	          + "                    \"category\": { "
-//	          + "                        \"coding\": [ "
-//	          + "                            { "
-//	          + "                                \"system\": \"http://argonaut.hl7.org\", "
-//	          + "                                \"code\": \"problem\" "
-//	          + "                            } "
-//	          + "                        ] "
-//	          + "                    }, "
-//	          + "                    \"clinicalStatus\": \"active\", "
-//	          + "                    \"verificationStatus\": \"unknown\", "
-//	          + "                    \"dateRecorded\": \"2013-04-14\", "
-//	          + "                    \"onsetDateTime\": \"2013-04-15T01:15:52Z\" "
-//	          + "                }, "
-//	          + "                \"search\": { "
-//	          + "                    \"mode\": \"match\" "
-//	          + "                } "
-//	          + "            } "
-//	          + "        } "
-//	          + "    ] "
-//	          + "} ";
 }
