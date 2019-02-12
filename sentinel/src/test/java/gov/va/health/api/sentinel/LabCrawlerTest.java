2f0a7f2e7496f324f00571de37a01c9c189c5e14
Emily Subocz <emily.subocz@libertyits.com>
2019-02-12 14:33:06 -0500

    Add patient-id command line argument logic

--- sentinel/src/test/java/gov/va/health/api/sentinel/LabCrawlerTest.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/LabCrawlerTest.java
@@ -1,5 +1,6 @@
 package gov.va.health.api.sentinel;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import gov.va.health.api.sentinel.categories.NotInLab;
@@ -11,6 +12,7 @@ import gov.va.health.api.sentinel.crawler.FileResultsCollector;
 import gov.va.health.api.sentinel.crawler.RequestQueue;
 import gov.va.health.api.sentinel.crawler.ResourceDiscovery;
 import gov.va.health.api.sentinel.crawler.SummarizingResultCollector;
+import gov.va.health.api.sentinel.crawler.UrlReplacementRequestQueue;
 import java.io.File;
 import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
@@ -22,8 +24,27 @@ public class LabCrawlerTest {
 
   private LabRobots robots = LabRobots.fromSystemProperties();
 
-  private void crawl(IdMeOauthRobot robot) {
-    Swiggity.swooty(robot.config().user().id());
+  @Category({NotInLocal.class, NotInLab.class, NotInProd.class})
+  @Test
+  public void crawl() {
+    SystemDefinition env = Sentinel.get().system();
+    String patient = System.getProperty("patient-id", "vasdvp+IDME_01@gmail.com");
+    IdMeOauthRobot robot;
+    if (patient.equals("vasdvp+IDME_01@gmail.com")) {
+      robot = robots.user1();
+    } else if (patient.equals("vasdvp+IDME_02@gmail.com")) {
+      robot = robots.user2();
+    } else if (patient.equals("vasdvp+IDME_03@gmail.com")) {
+      robot = robots.user3();
+    } else if (patient.equals("vasdvp+IDME_04@gmail.com")) {
+      robot = robots.user4();
+    } else if (patient.equals("vasdvp+IDME_05@gmail.com")) {
+      robot = robots.user5();
+    } else {
+      throw new IllegalArgumentException(patient + " is not a valid patient id.");
+    }
+    Swiggity.swooty(patient);
+
     assertThat(robot.token().accessToken()).isNotBlank();
     ResourceDiscovery discovery =
         ResourceDiscovery.builder()
@@ -33,7 +54,8 @@ public class LabCrawlerTest {
     SummarizingResultCollector results =
         SummarizingResultCollector.wrap(
             new FileResultsCollector(new File("target/lab-crawl-" + robot.token().patient())));
-    RequestQueue q = new ConcurrentRequestQueue();
+
+    RequestQueue q = requestQueue(env);
     discovery.queries().forEach(q::add);
     Crawler crawler =
         Crawler.builder()
@@ -52,33 +74,18 @@ public class LabCrawlerTest {
     assertThat(results.failures()).withFailMessage("%d Failures", results.failures()).isEqualTo(0);
   }
 
-  @Category({NotInLocal.class, NotInLab.class, NotInProd.class})
-  @Test
-  public void crawlUser1() {
-    crawl(robots.user1());
-  }
-
-  @Category({NotInLocal.class, NotInLab.class, NotInProd.class})
-  @Test
-  public void crawlUser2() {
-    crawl(robots.user2());
-  }
-
-  @Category({NotInLocal.class, NotInLab.class, NotInProd.class})
-  @Test
-  public void crawlUser3() {
-    crawl(robots.user3());
-  }
-
-  @Category({NotInLocal.class, NotInLab.class, NotInProd.class})
-  @Test
-  public void crawlUser4() {
-    crawl(robots.user4());
-  }
-
-  @Category({NotInLocal.class, NotInLab.class, NotInProd.class})
-  @Test
-  public void crawlUser5() {
-    crawl(robots.user5());
+  private RequestQueue requestQueue(SystemDefinition env) {
+    String replaceUrl = System.getProperty("sentinel.argonaut.url.replace");
+    if (isBlank(replaceUrl)) {
+      log.info("Link replacement disabled (Override with -Dsentinel.argonaut.url.replace=<url>)");
+      return new ConcurrentRequestQueue();
+    }
+    log.info(
+        "Link replacement {} (Override with -Dsentinel.argonaut.url.replace=<url>)", replaceUrl);
+    return UrlReplacementRequestQueue.builder()
+        .replaceUrl(replaceUrl)
+        .withUrl(env.argonaut().url())
+        .requestQueue(new ConcurrentRequestQueue())
+        .build();
   }
 }
