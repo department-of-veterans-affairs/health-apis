96e8f523eaad6aaa5d88c864115c60b6cfdf2804
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-02-14 12:49:30 -0500

    Rollback threads inside ResourceVerifier.

--- sentinel/src/test/java/gov/va/health/api/sentinel/ResourceVerifier.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/ResourceVerifier.java
@@ -1,20 +1,11 @@
 package gov.va.health.api.sentinel;
 
-import static org.apache.commons.lang3.StringUtils.isNotBlank;
-import static org.assertj.core.api.Assertions.assertThat;
-
 import gov.va.api.health.argonaut.api.bundle.AbstractBundle;
 import gov.va.api.health.argonaut.api.resources.OperationOutcome;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -41,8 +32,6 @@ public class ResourceVerifier {
   private final Set<Class<?>> verifiedPageBoundsClasses =
       Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-  private final ExecutorService executorService = Executors.newFixedThreadPool(threadCount());
-
   public static ResourceVerifier get() {
     return INSTANCE;
   }
@@ -55,20 +44,6 @@ public class ResourceVerifier {
         .response(response)
         .status(status)
         .build();
-  }
-
-  private static int threadCount() {
-    int threads = Runtime.getRuntime().availableProcessors();
-    String maybeNumber = System.getProperty("sentinel.threads");
-    if (isNotBlank(maybeNumber)) {
-      try {
-        threads = Integer.parseInt(maybeNumber);
-      } catch (NumberFormatException e) {
-        log.warn("Bad thread count {}, assuming {}", maybeNumber, threads);
-      }
-    }
-    log.info("Using {} threads (Override with -Dsentinel.threads=<number>)", threads);
-    return threads;
   }
 
   /**
@@ -117,26 +92,17 @@ public class ResourceVerifier {
 
   @SneakyThrows
   public void verifyAll(TestCase<?>... testCases) {
-    List<Future<?>> futures = new ArrayList<>(testCases.length);
     for (TestCase<?> tc : testCases) {
-      futures.add(
-          executorService.submit(
-              () -> {
-                try {
-                  return verify(tc);
-                } catch (Exception | AssertionError e) {
-                  log.error(
-                      "Failure: {} with parameters {}: {}",
-                      tc.path(),
-                      Arrays.toString(tc.parameters()),
-                      e.getMessage());
-                  throw e;
-                }
-              }));
-    }
-
-    for (Future<?> future : futures) {
-      assertThat(future.get(5, TimeUnit.MINUTES)).isNotNull();
+      try {
+        verify(tc);
+      } catch (Exception | AssertionError e) {
+        log.error(
+            "Failure: {} with parameters {}: {}",
+            tc.path(),
+            Arrays.toString(tc.parameters()),
+            e.getMessage());
+        throw e;
+      }
     }
   }
 
