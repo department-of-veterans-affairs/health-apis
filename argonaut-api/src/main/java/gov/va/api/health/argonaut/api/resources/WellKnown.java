897eb7691283bbbfce843d4f3ba248fccd21d8cc
jjennings <joshua.jennings@libertyits.com>
2019-02-21 14:36:27 -0500

    *imports is bad

--- argonaut-api/src/main/java/gov/va/api/health/argonaut/api/resources/WellKnown.java
+++ argonaut-api/src/main/java/gov/va/api/health/argonaut/api/resources/WellKnown.java
@@ -1,11 +1,17 @@
 package gov.va.api.health.argonaut.api.resources;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import gov.va.api.health.argonaut.api.Fhir;
+import gov.va.api.health.argonaut.api.elements.Meta;
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.*;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
@@ -13,7 +19,7 @@ import javax.validation.constraints.NotEmpty;
 @AllArgsConstructor
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @Schema(description = "Well-Known")
-public class WellKnown{
+public final class WellKnown {
 
   @NotBlank String authorizationEndpoint;
   @NotBlank String tokenEndpoint;
@@ -25,5 +31,4 @@ public class WellKnown{
   String introspectionEndpoint;
   String revocationEndpoint;
   @NotEmpty String[] capabilities;
-
 }
