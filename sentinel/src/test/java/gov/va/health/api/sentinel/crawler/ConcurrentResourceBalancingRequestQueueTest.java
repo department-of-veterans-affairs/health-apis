dbbe4c45534eeb4a8fe0f7eb4128e30c884d0819
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-02-25 09:16:54 -0500

    Rename unit test.

--- sentinel/src/test/java/gov/va/health/api/sentinel/crawler/ConcurrentResourceBalancingRequestQueueTest.java
+++ sentinel/src/test/java/gov/va/health/api/sentinel/crawler/ConcurrentResourceBalancingRequestQueueTest.java
@@ -10,6 +10,49 @@ public final class ConcurrentResourceBalancingRequestQueueTest {
   ConcurrentResourceBalancingRequestQueue q = new ConcurrentResourceBalancingRequestQueue();
 
   @Test
+  public void balanceResources() {
+    final List<String> urls =
+        asList(
+            "foo/api/AllergyIntolerance/1",
+            "foo/api/Condition/1",
+            "foo/api/Condition?patient=bobnelson",
+            "foo/api/Condition",
+            "foo/api/Location/3",
+            "foo/api/Encounter/2",
+            "foo/api/Location/1",
+            "foo/api/Encounter/1",
+            "foo/api/Location/2");
+    for (String url : urls) {
+      q.add(url);
+    }
+    assertThat(q.next()).isEqualTo("foo/api/Condition?patient=bobnelson");
+    // As a result of the condition search, add page 2 and several condition reads.
+    q.add("foo/api/Condition?patient=bobnelson&page=2");
+    q.add("foo/api/Condition/2");
+    q.add("foo/api/Condition/3");
+    q.add("foo/api/Condition/4");
+    // Reads for all of the other resources should now be exhausted before returning to condition.
+    final List<String> expectedOrder =
+        asList(
+            "foo/api/AllergyIntolerance/1",
+            "foo/api/Encounter/1",
+            "foo/api/Location/1",
+            "foo/api/Encounter/2",
+            "foo/api/Location/2",
+            "foo/api/Location/3",
+            "foo/api/Condition?patient=bobnelson&page=2",
+            "foo/api/Condition",
+            "foo/api/Condition/1",
+            "foo/api/Condition/2",
+            "foo/api/Condition/3",
+            "foo/api/Condition/4");
+    for (String expectedUrl : expectedOrder) {
+      assertThat(q.next()).isEqualTo(expectedUrl);
+    }
+    assertThat(q.hasNext()).isFalse();
+  }
+
+  @Test
   public void duplicateItemsIgnored() {
     q.add("a");
     q.add("b");
@@ -55,50 +98,5 @@ public final class ConcurrentResourceBalancingRequestQueueTest {
     assertThat(q.next()).isEqualTo("foo/api/Immunization?patient=bobnelson");
     assertThat(q.next()).isEqualTo("foo/api/Immunization");
     assertThat(q.next()).isEqualTo("foo/api/Immunization/abc");
-  }
-
-  @Test
-  public void searchesAreWeightedMoreHeavilyThanReads() {
-    final List<String> urls =
-        asList(
-            "foo/api/AllergyIntolerance/1",
-            "foo/api/Condition/1",
-            "foo/api/Condition?patient=bobnelson",
-            "foo/api/Condition",
-            "foo/api/Location/3",
-            "foo/api/Encounter/2",
-            "foo/api/Location/1",
-            "foo/api/Encounter/1",
-            "foo/api/Location/2");
-    for (String url : urls) {
-      q.add(url);
-    }
-    assertThat(q.next()).isEqualTo("foo/api/Condition?patient=bobnelson");
-
-    // As a result of the condition search, add page 2 and several condition reads.
-    q.add("foo/api/Condition?patient=bobnelson&page=2");
-    q.add("foo/api/Condition/2");
-    q.add("foo/api/Condition/3");
-    q.add("foo/api/Condition/4");
-
-    // Reads for all of the other resources should now be exhausted before returning to condition.
-    final List<String> expectedOrder =
-        asList(
-            "foo/api/AllergyIntolerance/1",
-            "foo/api/Encounter/1",
-            "foo/api/Location/1",
-            "foo/api/Encounter/2",
-            "foo/api/Location/2",
-            "foo/api/Location/3",
-            "foo/api/Condition?patient=bobnelson&page=2",
-            "foo/api/Condition",
-            "foo/api/Condition/1",
-            "foo/api/Condition/2",
-            "foo/api/Condition/3",
-            "foo/api/Condition/4");
-    for (String expectedUrl : expectedOrder) {
-      assertThat(q.next()).isEqualTo(expectedUrl);
-    }
-    assertThat(q.hasNext()).isFalse();
   }
 }
