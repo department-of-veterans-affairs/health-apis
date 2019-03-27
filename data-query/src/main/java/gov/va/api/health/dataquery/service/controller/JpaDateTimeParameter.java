b01175d96aea6c49993f72fe4a1154d4cec40a73
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-03-27 10:22:52 -0400

    Review feedback.

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/JpaDateTimeParameter.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/JpaDateTimeParameter.java
@@ -18,6 +18,22 @@ import lombok.extern.slf4j.Slf4j;
 @Value
 @Getter(AccessLevel.PRIVATE)
 public final class JpaDateTimeParameter {
+  private static final int YEAR = 4;
+
+  private static final int YEAR_MONTH = 7;
+
+  private static final int YEAR_MONTH_DAY = 10;
+
+  private static final int YEAR_MONTH_DAY_HOUR = 13;
+
+  private static final int YEAR_MONTH_DAY_HOUR_MINUTE = 16;
+
+  private static final int YEAR_MONTH_DAY_HOUR_MINUTE_SECOND = 19;
+
+  private static final int TIME_ZONE = 20;
+
+  private static final int TIME_ZONE_OFFSET = 25;
+
   int index;
 
   SearchPrefix prefix;
@@ -56,6 +72,10 @@ public final class JpaDateTimeParameter {
     }
   }
 
+  private static UnsupportedOperationException apException() {
+    return new UnsupportedOperationException("AP search prefix not implemented");
+  }
+
   /** Build a combined JPA query snippet representing all the date criteria. */
   public static String querySnippet(String[] dates) {
     if (dates == null || dates.length == 0) {
@@ -72,51 +92,61 @@ public final class JpaDateTimeParameter {
     Instant lowerBound = lowerBound();
     Instant upperBound = upperBound();
     log.info("Date {} has bounds: [{}, {}]", date(), lowerBound, upperBound);
+
     switch (prefix()) {
       case EQ:
+        // falls through
       case NE:
+        // falls through
       case GE:
+        // falls through
       case LE:
         query.setParameter(placeholderLowerBound(), lowerBound);
         query.setParameter(placeholderUpperBound(), upperBound);
         return;
+
       case GT:
+        // falls through
       case SA:
         query.setParameter(placeholderUpperBound(), upperBound);
         return;
+
       case LT:
+        // falls through
       case EB:
         query.setParameter(placeholderLowerBound(), lowerBound);
         return;
+
       case AP:
-        throw new UnsupportedOperationException();
+        throw apException();
+
       default:
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("Unknown search prefix: " + prefix());
     }
   }
 
   private Instant lowerBound() {
     ZoneOffset offset = ZonedDateTime.now().getOffset();
     switch (date().length()) {
-      case 4:
+      case YEAR:
         return OffsetDateTime.parse(String.format("%s-01-01T00:00:00%s", date(), offset))
             .toInstant();
-      case 7:
+      case YEAR_MONTH:
         return OffsetDateTime.parse(String.format("%s-01T00:00:00%s", date(), offset)).toInstant();
-      case 10:
+      case YEAR_MONTH_DAY:
         return OffsetDateTime.parse(String.format("%sT00:00:00%s", date(), offset)).toInstant();
-      case 13:
+      case YEAR_MONTH_DAY_HOUR:
         return OffsetDateTime.parse(String.format("%s:00:00%s", date(), offset)).toInstant();
-      case 16:
+      case YEAR_MONTH_DAY_HOUR_MINUTE:
         return OffsetDateTime.parse(String.format("%s:00%s", date(), offset)).toInstant();
-      case 19:
+      case YEAR_MONTH_DAY_HOUR_MINUTE_SECOND:
         return OffsetDateTime.parse(date() + offset).toInstant();
-      case 20:
-        // falls through
-      case 25:
+      case TIME_ZONE:
+        return Instant.parse(date());
+      case TIME_ZONE_OFFSET:
         return OffsetDateTime.parse(date()).toInstant();
       default:
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("Cannot compute lower bound for date " + date());
     }
   }
 
@@ -135,37 +165,46 @@ public final class JpaDateTimeParameter {
         return String.format(
             " and :%s <= dr.effectiveDateTime and dr.issuedDateTime <= :%s",
             placeholderLowerBound(), placeholderUpperBound());
+
       case NE:
         // the range of the search value does not fully contain the range of the target value
         return String.format(
             " and (dr.effectiveDateTime < :%s or :%s < dr.issuedDateTime)",
             placeholderLowerBound(), placeholderUpperBound());
+
       case GT:
         // the range above the search value intersects the range of the target value
         return String.format(" and :%s < dr.issuedDateTime", placeholderUpperBound());
+
       case LT:
         // the range below the search value intersects the range of the target value
         return String.format(" and dr.effectiveDateTime < :%s", placeholderLowerBound());
+
       case GE:
         // or the range of the search value fully contains the range of the target value
         return String.format(
             " and (:%s <= dr.effectiveDateTime or :%s < dr.issuedDateTime)",
             placeholderLowerBound(), placeholderUpperBound());
+
       case LE:
         // or the range of the search value fully contains the range of the target value
         return String.format(
             " and (dr.effectiveDateTime < :%s or dr.issuedDateTime <= :%s)",
             placeholderLowerBound(), placeholderUpperBound());
+
       case SA:
         // and the range above the search value contains the range of the target value
         return String.format(" and :%s < dr.effectiveDateTime", placeholderUpperBound());
+
       case EB:
         // and the range below the search value contains the range of the target value
         return String.format(" and dr.issuedDateTime < :%s", placeholderLowerBound());
+
       case AP:
-        throw new UnsupportedOperationException();
+        throw apException();
+
       default:
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("Unknown search prefix: " + prefix());
     }
   }
 
@@ -173,24 +212,24 @@ public final class JpaDateTimeParameter {
     OffsetDateTime lowerBound =
         OffsetDateTime.ofInstant(lowerBound(), ZonedDateTime.now().getOffset());
     switch (date().length()) {
-      case 4:
+      case YEAR:
         return lowerBound.plusYears(1).minus(1, ChronoUnit.MILLIS).toInstant();
-      case 7:
+      case YEAR_MONTH:
         return lowerBound.plusMonths(1).minus(1, ChronoUnit.MILLIS).toInstant();
-      case 10:
+      case YEAR_MONTH_DAY:
         return lowerBound.plusDays(1).minus(1, ChronoUnit.MILLIS).toInstant();
-      case 13:
+      case YEAR_MONTH_DAY_HOUR:
         return lowerBound.plusHours(1).minus(1, ChronoUnit.MILLIS).toInstant();
-      case 16:
+      case YEAR_MONTH_DAY_HOUR_MINUTE:
         return lowerBound.plusMinutes(1).minus(1, ChronoUnit.MILLIS).toInstant();
-      case 19:
+      case YEAR_MONTH_DAY_HOUR_MINUTE_SECOND:
         // falls through
-      case 20:
+      case TIME_ZONE:
         // falls through
-      case 25:
+      case TIME_ZONE_OFFSET:
         return lowerBound.plusSeconds(1).minus(1, ChronoUnit.MILLIS).toInstant();
       default:
-        throw new IllegalArgumentException();
+        throw new IllegalArgumentException("Cannot compute upper bound for date " + date());
     }
   }
 
