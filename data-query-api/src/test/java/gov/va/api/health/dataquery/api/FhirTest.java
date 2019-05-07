a2813d8811b5d7f8976a3ecd5cee693cda1c5ff9
Patrick Scorer <patrick.scorer@libertyits.com>
2019-05-06 22:18:20 -0400

    made new regex reject empty strings like the old one did and the newest fhir spec does. Updated test cases for those regex changes

--- data-query-api/src/test/java/gov/va/api/health/dataquery/api/FhirTest.java
+++ data-query-api/src/test/java/gov/va/api/health/dataquery/api/FhirTest.java
@@ -10,27 +10,40 @@ import org.junit.Test;
 public class FhirTest {
 
   @Test
-  public void base64RegexMatches() {
-    assertThat(Pattern.matches(Fhir.BASE64, "SSBhdGUgbWFueSBwZWFudXRz"))
-        .withFailMessage("I ate many peanuts; failed valid BASE64 without padding.")
-        .isTrue();
-    assertThat(Pattern.matches(Fhir.BASE64, "SSBqdXN0IGF0ZSBhIHBlYW51dAo="))
-        .withFailMessage("I ate a peanut; failed valid BASE64 with one = padding.")
-        .isTrue();
-    assertThat(Pattern.matches(Fhir.BASE64, "SSBhdGUgYSBmZXcgcGVhbnV0cw=="))
-        .withFailMessage("I ate a few peanuts; failed valid BASE64 with two = padding.")
-        .isTrue();
+  public void base64RegexDoesNotMatchBadStrings() {
+    // Make sure that = is only allowed as the last 1 or 2 characters of the string.
+    assertThat(Pattern.matches(Fhir.BASE64, "SSBqdXN0IGF0ZSBhIHBlYW51dA=o"))
+        .withFailMessage("Padding character in wrong location is invalid but was not rejected.")
+        .isFalse();
     assertThat(Pattern.matches(Fhir.BASE64, ""))
-        .withFailMessage("Failed valid BASE64 empty string.")
-        .isTrue();
-    assertThat(
-            Pattern.matches(
-                Fhir.BASE64, "SSBqdXN0IGF0ZSBhIHBlYW51dAo=SSBqdXN0IGF0ZSBhIHBlYW51dAo="))
-        .withFailMessage("Invalid BASE64. = in middle of string was not rejected.")
+        .withFailMessage("Empty string is invalid but was not rejected.")
         .isFalse();
     assertThat(Pattern.matches(Fhir.BASE64, "SSBqdXN0IGF0ZSBhIHBlYW51dAo"))
-        .withFailMessage("Invalid BASE64. String missing padding was not rejected.")
+        .withFailMessage("Missing padding/improper size is invalid but was not rejected.")
         .isFalse();
+  }
+
+  @Test
+  public void base64RegexMatchesGoodStrings() {
+    // There are two separate cases in the regex that should be tested. < and > 4 characters.
+    assertThat(Pattern.matches(Fhir.BASE64, "QUJD"))
+        .withFailMessage("Failed short, valid BASE64 without padding.")
+        .isTrue();
+    assertThat(Pattern.matches(Fhir.BASE64, "QUI="))
+        .withFailMessage("Failed short, valid BASE64 with one padding char.")
+        .isTrue();
+    assertThat(Pattern.matches(Fhir.BASE64, "QQ=="))
+        .withFailMessage("Failed short, valid BASE64 with two padding chars.")
+        .isTrue();
+    assertThat(Pattern.matches(Fhir.BASE64, "SSBhdGUgbWFueSBwZWFudXRz"))
+        .withFailMessage("Failed long, valid BASE64 without padding.")
+        .isTrue();
+    assertThat(Pattern.matches(Fhir.BASE64, "SSBqdXN0IGF0ZSBhIHBlYW51dAo="))
+        .withFailMessage("Failed long, valid BASE64 with one padding char.")
+        .isTrue();
+    assertThat(Pattern.matches(Fhir.BASE64, "SSBhdGUgYSBmZXcgcGVhbnV0cw=="))
+        .withFailMessage("Failed long, valid BASE64 with two padding chars.")
+        .isTrue();
   }
 
   /**
@@ -41,23 +54,23 @@ public class FhirTest {
   public void parseDateTime() {
     assertThat(Fhir.parseDateTime(null)).isNull();
     assertThat(Fhir.parseDateTime(" ")).isNull();
-    for (String datetime :
-        Arrays.asList( // dateTime
+    for (String datetime : // dateTime
+        Arrays.asList(
             "2002-05-30T09:00:00",
             "2002-05-30T09:30:10.5",
             "2002-05-30T09:30:10Z",
-            "2002-05-30T09:30:10-06:00",
-            "2002-05-30T09:30:10+06:00", // date
+            "2002-05-30T09:30:10-06:00", // date
+            "2002-05-30T09:30:10+06:00",
             "2002-09-24",
             "2002-09-24Z",
-            "2002-09-24-06:00",
-            "2002-09-24+06:00", // gYearMonth
+            "2002-09-24-06:00", // gYearMonth
+            "2002-09-24+06:00",
             "2001-10",
             "2001-10+02:00",
             "2001-10Z",
             "2001-10+00:00",
-            "-2001-10",
-            "-20000-04", // gYear
+            "-2001-10", // gYear
+            "-20000-04",
             "2001",
             "2001+02:00",
             "2001Z",
