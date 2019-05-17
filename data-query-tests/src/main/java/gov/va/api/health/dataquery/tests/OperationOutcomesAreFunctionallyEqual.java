375d53555db59eaeda8fc12db0ccc265deb2779b
Patrick Scorer <patrick.scorer@libertyits.com>
2019-05-17 16:27:54 -0400

    rename after a quick review

--- data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/OperationOutcomesAreFunctionallyEqual.java
+++ data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/OperationOutcomesAreFunctionallyEqual.java
@@ -9,6 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 public class OperationOutcomesAreFunctionallyEqualCheck
     implements ResponsesAreFunctionallyEqualCheck {
 
+  @Override
+  public boolean equals(ResponseBody<?> responseBody1, ResponseBody<?> responseBody2) {
+    OperationOutcome oo1 = asOperationOutcomeWithoutDiagnostics(responseBody1);
+    OperationOutcome oo2 = asOperationOutcomeWithoutDiagnostics(responseBody2);
+    return oo1.equals(oo2);
+  }
+
   /**
    * Remove data from the OO that is unique for each instance. This includes the generated ID and
    * the timestamp.
@@ -31,12 +38,5 @@ public class OperationOutcomesAreFunctionallyEqualCheck
       log.error("Failed read response as OperationOutcome: {}", body.prettyPrint());
       throw e;
     }
-  }
-
-  @Override
-  public boolean equals(ResponseBody<?> responseBody1, ResponseBody<?> responseBody2) {
-    OperationOutcome oo1 = asOperationOutcomeWithoutDiagnostics(responseBody1);
-    OperationOutcome oo2 = asOperationOutcomeWithoutDiagnostics(responseBody2);
-    return oo1.equals(oo2);
   }
 }
