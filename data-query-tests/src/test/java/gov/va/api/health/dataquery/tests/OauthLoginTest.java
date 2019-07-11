da6bf24fc13417ac3708e65c2623dd8eaadaa8e4
jtorano <joseph.torano@libertyits.com>
2019-07-11 11:11:53 -0400

    updated oathTests in relation to changes made in LabBot

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/OauthLoginTest.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/OauthLoginTest.java
@@ -7,9 +7,9 @@ import gov.va.api.health.sentinel.LabBot.LabBotUserResult;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.SneakyThrows;
@@ -19,9 +19,47 @@ import org.junit.Test;
 @Slf4j
 public class OauthLoginTest {
 
-  private List<String> winners = new CopyOnWriteArrayList<>();
-
-  private List<String> losers = new CopyOnWriteArrayList<>();
+  @Test
+  @SneakyThrows
+  public void RequestTest() {
+    List<LabBotUserResult> labBotUserResultList =
+        LabBot.builder()
+            .userIds(LabBot.allUsers())
+            .scopes(dataQueryScopes())
+            .configFile("config/lab.properties")
+            .build()
+            .request("/services/fhir/v0/dstu2/Patient/{icn}");
+    List<String> winners = new ArrayList<>();
+    List<String> losers = new ArrayList<>();
+    for (LabBotUserResult labBotUserResult : labBotUserResultList) {
+      if (!labBotUserResult.tokenExchange().isError()
+          && labBotUserResult.response().contains("\"resourceType\":\"Patient\"")) {
+        log.info(
+            "Winner: {} is patient {}.",
+            labBotUserResult.user().id(),
+            labBotUserResult.tokenExchange().patient());
+        winners.add(labBotUserResult.user().id());
+      } else {
+        log.info(
+            "Loser: {} is patient {}.",
+            labBotUserResult.user().id(),
+            labBotUserResult.tokenExchange().patient());
+        losers.add(
+            labBotUserResult.user().id()
+                + " - "
+                + labBotUserResult.tokenExchange().error()
+                + ": "
+                + labBotUserResult.tokenExchange().errorDescription());
+      }
+    }
+    String report =
+        Stream.concat(winners.stream().map(w -> w + " - OK"), losers.stream())
+            .sorted()
+            .collect(Collectors.joining("\n"));
+    Files.write(new File("lab-users.txt").toPath(), report.getBytes(StandardCharsets.UTF_8));
+    log.info("Lab Users:\n{}", report);
+    assertThat(losers.size()).isZero();
+  }
 
   public static List<String> dataQueryScopes() {
     return Arrays.asList(
@@ -34,50 +72,10 @@ public class OauthLoginTest {
         "patient/MedicationStatement.read",
         "patient/Observation.read",
         "patient/Patient.read",
-        "patient/Procedure.read");
-  }
-
-  @Test
-  @SneakyThrows
-  public void RequestTest() {
-    List<LabBotUserResult> labBotUserResultList =
-        LabBot.builder()
-            .userIds(LabBot.allUsers())
-            .scopes(dataQueryScopes())
-            .configPathName("config/lab.properties")
-            .build()
-            .request("/services/fhir/v0/dstu2/Patient/");
-    winnersLosers(labBotUserResultList);
-    String report =
-        Stream.concat(winners.stream().map(w -> w + " - OK"), losers.stream())
-            .sorted()
-            .collect(Collectors.joining("\n"));
-    Files.write(new File("lab-users.txt").toPath(), report.getBytes(StandardCharsets.UTF_8));
-    log.info("Lab Users:\n{}", report);
-    assertThat(losers.size()).isZero();
-  }
-
-  private void winnersLosers(List<LabBotUserResult> labBotUserResultList) {
-    for (LabBotUserResult labBotUserResult : labBotUserResultList) {
-      if (!labBotUserResult.tokenExchange.isError()
-          && labBotUserResult.response.contains("\"resourceType\":\"Patient\"")) {
-        log.info(
-            "Winner: {} is patient {}.",
-            labBotUserResult.user.id(),
-            labBotUserResult.tokenExchange.patient());
-        winners.add(labBotUserResult.user.id());
-      } else {
-        log.info(
-            "Loser: {} is patient {}.",
-            labBotUserResult.user.id(),
-            labBotUserResult.tokenExchange.patient());
-        losers.add(
-            labBotUserResult.user.id()
-                + " - "
-                + labBotUserResult.tokenExchange.error()
-                + ": "
-                + labBotUserResult.tokenExchange.errorDescription());
-      }
-    }
+        "patient/Procedure.read",
+        "openid",
+        "profile",
+        "offline_access",
+        "launch/patient");
   }
 }
