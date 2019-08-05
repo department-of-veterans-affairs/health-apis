55735fd8d5b920cbd219bfac257c37ec1b3a0fb6
Bryan Schofield <bryan.schofield@libertyits.com>
2019-08-05 15:33:50 -0400

    wip - datamart immunization controller

---
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/immunization/ImmuizationRepository.java
@@ -0,0 +1,10 @@
+package gov.va.api.health.dataquery.service.controller.immunization;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ImmuizationRepository
+    extends PagingAndSortingRepository<ImmunizationEntity, String> {
+  Page<ImmunizationEntity> findByIcn(String icn, Pageable pageable);
+}
---
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/immunization/DatamartImmunizationControllerTest.java
@@ -0,0 +1,3 @@
+package gov.va.api.health.dataquery.service.controller.immunization;
+
+public class DatamartImmunizationControllerTest {}
