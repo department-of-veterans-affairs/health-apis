60df581084276d6a9d0aef2a51046fc35b029736
Bryan Schofield <bryan.schofield@libertyits.com>
2019-08-06 14:19:39 -0400

    formatting

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/immunization/DatamartImmunizationSamples.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/immunization/DatamartImmunizationSamples.java
@@ -111,7 +111,8 @@ class DatamartImmunizationSamples {
           .total(conditions.size())
           .link(Arrays.asList(links))
           .entry(
-              conditions.stream()
+              conditions
+                  .stream()
                   .map(
                       c ->
                           Entry.builder()
