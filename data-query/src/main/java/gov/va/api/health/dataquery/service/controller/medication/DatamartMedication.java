235cad497e33877b4eae32999ff591d2edff18cc
eclendenning-va <evan.clendenning@libertyits.com>
2019-08-06 16:00:15 -0400

    formatting

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medication/DatamartMedication.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medication/DatamartMedication.java
@@ -1,11 +1,10 @@
 package gov.va.api.health.dataquery.service.controller.medication;
 
-
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
 
 @Data
 @Builder
@@ -13,36 +12,35 @@ import lombok.AccessLevel;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class DatamartMedication {
 
-    @Builder.Default private String objectType = "Medication";
+  @Builder.Default private String objectType = "Medication";
 
-    @Builder.Default private String objectVersion = "1";
+  @Builder.Default private String objectVersion = "1";
 
-    private String cdwId;
+  private String cdwId;
 
-    private String etlDate;
+  private String etlDate;
 
-    private RxNorm rxnorm;
+  private RxNorm rxnorm;
 
-    private Product product;
+  private Product product;
 
-    @Data
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class RxNorm {
-        String code;
+  @Data
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class RxNorm {
+    String code;
 
-        String text;
-    }
+    String text;
+  }
 
-    @Data
-    @Builder
-    @NoArgsConstructor(access = AccessLevel.PRIVATE)
-    @AllArgsConstructor(access = AccessLevel.PRIVATE)
-    public static class Product {
-        String id;
+  @Data
+  @Builder
+  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  public static class Product {
+    String id;
 
-        String formText;
-    }
-
+    String formText;
+  }
 }
