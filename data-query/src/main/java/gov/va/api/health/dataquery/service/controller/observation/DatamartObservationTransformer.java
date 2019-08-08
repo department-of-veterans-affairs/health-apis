aefe84981b272b2f2edc8be23ee08691412f24c0
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-08-08 11:32:44 -0400

    .

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/observation/DatamartObservationTransformer.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/observation/DatamartObservationTransformer.java
@@ -1,6 +1,7 @@
 package gov.va.api.health.dataquery.service.controller.observation;
 
 import gov.va.api.health.argonaut.api.resources.Observation;
+import gov.va.api.health.dataquery.service.controller.EnumSearcher;
 import lombok.Builder;
 import lombok.NonNull;
 
@@ -8,26 +9,36 @@ import lombok.NonNull;
 final class DatamartObservationTransformer {
   @NonNull final DatamartObservation datamart;
 
+  private static Observation.Status status(DatamartObservation.Status status) {
+    if (status == null) {
+      return null;
+    }
+    return EnumSearcher.of(Observation.Status.class).find(status.toString());
+  }
+
   Observation toFhir() {
-    //    .status(status(cdw.getStatus()))
-    //    .category(category(cdw.getCategory()))
-    //    .code(code(cdw.getCode()))
-    //    .subject(reference(cdw.getSubject()))
-    //    .encounter(reference(cdw.getEncounter()))
-    //    .effectiveDateTime(asDateTimeString(cdw.getEffectiveDateTime()))
-    //    .issued(asDateTimeString(cdw.getIssued()))
-    //    .performer(performers(cdw.getPerformers()))
-    //    .valueQuantity(valueQuantity(cdw.getValueQuantity()))
-    //    .valueCodeableConcept(valueCodeableConcept(cdw.getValueCodeableConcept()))
-    //    .interpretation(interpretation(cdw.getInterpretation()))
-    //    .comments(cdw.getComments())
-    //    .referenceRange(referenceRanges(cdw.getReferenceRanges(), cdw.getCategory()))
-    //    .component(components(cdw.getComponents()))
+    //    .category(category(cdw.Category()))
+    //    .code(code(cdw.Code()))
+    //    .subject(reference(cdw.Subject()))
+    //    .encounter(reference(cdw.Encounter()))
+    //    .effectiveDateTime(asDateTimeString(cdw.EffectiveDateTime()))
+    //    .issued(asDateTimeString(cdw.Issued()))
+    //    .performer(performers(cdw.Performers()))
+    //    .valueQuantity(valueQuantity(cdw.ValueQuantity()))
+    //    .valueCodeableConcept(valueCodeableConcept(cdw.ValueCodeableConcept()))
+    //    .interpretation(interpretation(cdw.Interpretation()))
+    //    .comments(cdw.Comments())
+    //    .referenceRange(referenceRanges(cdw.ReferenceRanges(), cdw.Category()))
+    //    .component(components(cdw.Components()))
 
     /*
      * Specimen reference is omitted since we do not support the a specimen resource and
      * do not want dead links
      */
-    return Observation.builder().resourceType("Observation").id(datamart.cdwId()).build();
+    return Observation.builder()
+        .resourceType("Observation")
+        .id(datamart.cdwId())
+        .status(status(datamart.status()))
+        .build();
   }
 }
