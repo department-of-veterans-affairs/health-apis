0aebbe9c0070e9f3a274d85a0962c8beae64395a
jhulbert-va <joshua.hulbert@libertyits.com>
2019-08-08 09:28:12 -0400

    Compilation Errors

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medicationorder/DatamartMedicationOrder.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medicationorder/DatamartMedicationOrder.java
@@ -1,41 +1,45 @@
 package gov.va.api.health.dataquery.service.controller.medicationorder;
 
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import gov.va.api.health.dataquery.service.controller.datamart.DatamartReference;
 import gov.va.api.health.dataquery.service.controller.datamart.HasReplaceableId;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 @Data
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DatamartMedicationOrder implements HasReplaceableId {
-  @Builder.Default private String objectType = "MedicationOrder";
-  @Builder.Default private String objectVersion = "1";
-  private String cdwId;
-  private DatamartReference patient;
-  private Instant dateWritten;
-  private Status status;
-  private Optional<Instant> dateEnded;
-  private DatamartReference prescriber;
-  private DatamartReference medication;
-  private List<DosageInstruction> dosageInstruction;
-  private Optional<DispenseRequest> dispenseRequest;
 
-  /** Backwards compatibility for etlDate. */
-  private void setEtlDate(String unused) {
-    /* no op */
-  }
+  @Builder.Default private String objectType = "MedicationOrder";
+
+  @Builder.Default private String objectVersion = "1";
+
+  private String cdwId;
+
+  private DatamartReference patient;
+
+  private Instant dateWritten;
+
+  private Status status;
+
+  private Optional<Instant> dateEnded;
+
+  private DatamartReference prescriber;
+
+  private DatamartReference medication;
+
+  private List<DosageInstruction> dosageInstruction;
+
+  private Optional<DispenseRequest> dispenseRequest;
 
   /** Lazy initialization with empty. */
   public Optional<Instant> dateEnded() {
@@ -53,36 +57,41 @@ public class DatamartMedicationOrder implements HasReplaceableId {
     return dosageInstruction;
   }
 
+  /** Backwards compatibility for etlDate. */
+  private void setEtlDate(String unused) {
+    /* no op */
+  }
+
+  public enum Status {
+    completed,
+    stopped,
+    @JsonProperty(value = "on-hold")
+    on_hold,
+    active,
+    draft,
+    @JsonProperty(value = "entered-in-error")
+    entered_in_error
+  }
+
   @Data
   @Builder
   @AllArgsConstructor(access = AccessLevel.PRIVATE)
   @NoArgsConstructor(access = AccessLevel.PRIVATE)
-  public class DosageInstruction {
+  static final class DosageInstruction {
+
     private Optional<String> dosageText;
+
     private Optional<String> timingText;
+
     private Optional<String> additionalInstructions;
+
     private boolean asNeeded;
+
     private Optional<String> routeText;
+
     private Optional<Double> doseQuantityValue;
+
     private Optional<String> doseQuantityUnit;
-
-
-
-    /** Lazy initialization with empty. */
-    public Optional<String> dosageText(){
-      if (dosageText == null) {
-        return Optional.empty();
-      }
-      return dosageText;
-    }
-
-    /** Lazy initialization with empty. */
-    public Optional<String> timingText() {
-      if(timingText == null) {
-        return Optional.empty();
-      }
-      return timingText;
-    }
 
     /** Lazy initialization with empty. */
     public Optional<String> additionalInstructions() {
@@ -93,11 +102,19 @@ public class DatamartMedicationOrder implements HasReplaceableId {
     }
 
     /** Lazy initialization with empty. */
-    public Optional<String> routeText() {
-      if (routeText == null) {
+    public Optional<String> dosageText() {
+      if (dosageText == null) {
         return Optional.empty();
       }
-      return routeText;
+      return dosageText;
+    }
+
+    /** Lazy initialization with empty. */
+    public Optional<String> doseQuantityUnit() {
+      if (doseQuantityUnit == null) {
+        return Optional.empty();
+      }
+      return doseQuantityUnit;
     }
 
     /** Lazy initialization with empty. */
@@ -109,11 +126,19 @@ public class DatamartMedicationOrder implements HasReplaceableId {
     }
 
     /** Lazy initialization with empty. */
-    public Optional<String> doseQuantityUnit() {
-      if (doseQuantityUnit == null) {
+    public Optional<String> routeText() {
+      if (routeText == null) {
         return Optional.empty();
       }
-      return doseQuantityUnit;
+      return routeText;
+    }
+
+    /** Lazy initialization with empty. */
+    public Optional<String> timingText() {
+      if (timingText == null) {
+        return Optional.empty();
+      }
+      return timingText;
     }
   }
 
@@ -121,12 +146,25 @@ public class DatamartMedicationOrder implements HasReplaceableId {
   @Builder
   @AllArgsConstructor(access = AccessLevel.PRIVATE)
   @NoArgsConstructor(access = AccessLevel.PRIVATE)
-  public class DispenseRequest {
+  static final class DispenseRequest {
+
     private Optional<Integer> numberOfRepeatsAllowed;
+
     private Optional<Double> quantity;
+
     private Optional<String> unit;
+
     private Optional<Integer> expectedSupplyDuration;
+
     private Optional<String> supplyDurationUnits;
+
+    /** Lazy initialization with empty. */
+    public Optional<Integer> expectedSupplyDuration() {
+      if (expectedSupplyDuration == null) {
+        return Optional.empty();
+      }
+      return expectedSupplyDuration;
+    }
 
     /** Lazy initialization with empty. */
     public Optional<Integer> numberOfRepeatsAllowed() {
@@ -145,38 +183,19 @@ public class DatamartMedicationOrder implements HasReplaceableId {
     }
 
     /** Lazy initialization with empty. */
-    public Optional<String> unit() {
-      if (unit == null) {
-        return Optional.empty();
-      }
-      return unit;
-    }
-
-    /** Lazy initialization with empty. */
-    public Optional<Integer> expectedSupplyDuration() {
-      if (expectedSupplyDuration == null) {
-        return Optional.empty();
-      }
-      return expectedSupplyDuration;
-    }
-
-    /** Lazy initialization with empty. */
     public Optional<String> supplyDurationUnits() {
       if (supplyDurationUnits == null) {
         return Optional.empty();
       }
       return supplyDurationUnits;
     }
-  }
 
-  public enum Status {
-    completed,
-    stopped,
-    @JsonProperty(value = "on-hold")
-    on_hold,
-    active,
-    draft,
-    @JsonProperty(value = "entered-in-error")
-    entered_in_error
+    /** Lazy initialization with empty. */
+    public Optional<String> unit() {
+      if (unit == null) {
+        return Optional.empty();
+      }
+      return unit;
+    }
   }
 }
