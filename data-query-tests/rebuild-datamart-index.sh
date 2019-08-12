ac1d9d8b09dd9914ef7ebf0766cd5a9be49199a4
jhulbert-va <joshua.hulbert@libertyits.com>
2019-08-12 08:59:14 -0400

    Rebuilt datamart index

--- data-query-tests/rebuild-datamart-index.sh
+++ data-query-tests/rebuild-datamart-index.sh
@@ -31,6 +31,7 @@ do
     -e 's/\(MedicationStatement.*\)1400000182116/\1800000000707/' \
     -e 's/identifier=1000001782544/identifier=10000020531/' \
     -e 's/identifier=1400000140034/identifier=1000001259996/' \
+    -e 's/\(MedicationOrder.*\)1200389904206/\11400162277477/' \
     $SOURCE >> $DESTINATION
 done
 
