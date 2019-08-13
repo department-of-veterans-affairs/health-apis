b1d1f83c344b991e585e8a921b1e2cf4420b0f98
jhulbert-va <joshua.hulbert@libertyits.com>
2019-08-13 08:44:32 -0400

    Separate Manifestation CodeableConcepts

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/allergyintolerance/DatamartAllergyIntoleranceTransformer.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/allergyintolerance/DatamartAllergyIntoleranceTransformer.java
@@ -15,9 +15,9 @@ import gov.va.api.health.dstu2.api.datatypes.Annotation;
 import gov.va.api.health.dstu2.api.datatypes.CodeableConcept;
 import gov.va.api.health.dstu2.api.datatypes.Coding;
 import gov.va.api.health.dstu2.api.elements.Reference;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.Builder;
 import lombok.NonNull;
 
@@ -39,18 +39,15 @@ public final class DatamartAllergyIntoleranceTransformer {
     return EnumSearcher.of(AllergyIntolerance.Certainty.class).find(certainty.toString());
   }
 
-  private List<CodeableConcept> manifestations(List<DatamartCoding> manifestations) {
-    if (isEmpty(manifestations)) {
+  private List<CodeableConcept> manifestations(List<DatamartCoding> maybeManifestations) {
+    if (isEmpty(maybeManifestations)) {
       return null;
     }
-    return asList(
-        CodeableConcept.builder()
-            .coding(
-                manifestations
-                    .stream()
-                    .map(m -> asCoding(Optional.ofNullable(m)))
-                    .collect(Collectors.toList()))
-            .build());
+    List<CodeableConcept> manifestations = new ArrayList<>();
+    for (DatamartCoding manifestation : maybeManifestations) {
+      manifestations.add(CodeableConcept.builder().coding(asList(asCoding(manifestation))).build());
+    }
+    return emptyToNull(manifestations);
   }
 
   private Annotation notes(List<DatamartAllergyIntolerance.Note> notes) {
