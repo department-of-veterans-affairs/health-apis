b3b8dab24d711e769b97fa7e9a76c2264a57035f
jhulbert <joshua.hulbert@libertyits.com>
2019-08-20 09:38:04 -0400

    Minimart By Resource One time not each

--- data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/MitreMinimartMaker.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/MitreMinimartMaker.java
@@ -34,217 +34,261 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 
 @Slf4j
 public class MitreMinimartMaker {
 
   private static EntityManager entityManager;
 
-  /** Uses the resource name to determine what the prefix of the file should be. */
-  private static String determineFilePrefix(String resourceFullName) {
-    String abbreviation = "";
-    switch (resourceFullName) {
-      case "AllergyIntolerance":
-        abbreviation = "AllInt";
-        break;
-      case "Condition":
-        abbreviation = "Con";
-        break;
-      case "DiagnosticReport":
-        abbreviation = "DiaRep";
-        break;
-      case "Immunization":
-        abbreviation = "Imm";
-        break;
-      case "MedicationStatement":
-        abbreviation = "MedSta";
-        break;
-      case "MedicationOrder":
-        abbreviation = "MedOrd";
-        break;
-      case "Medication":
-        abbreviation = "Med";
-        break;
-      case "Observation":
-        abbreviation = "Obs";
-        break;
-      case "Patient":
-        abbreviation = "Pat";
-        break;
-      case "Procedure":
-        abbreviation = "Pro";
-        break;
-      default:
-        abbreviation = "--FAILURE--";
-    }
-    return "dm" + abbreviation;
+  @SneakyThrows
+  private static void insertByAllergyIntolerance(File file) {
+    DatamartAllergyIntolerance dm =
+        JacksonConfig.createMapper().readValue(file, DatamartAllergyIntolerance.class);
+    AllergyIntoleranceEntity entity =
+        AllergyIntoleranceEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(patientIcn(dm.patient().isPresent() ? dm.patient().get() : null))
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
   }
 
-  /** Uses the entity manager to insert each record into the database without committing. */
   @SneakyThrows
-  private static void insertByResourceType(String resourceName, File file) {
-    switch (resourceName) {
-      case "AllergyIntolerance":
-        DatamartAllergyIntolerance ai =
-            JacksonConfig.createMapper().readValue(file, DatamartAllergyIntolerance.class);
-        AllergyIntoleranceEntity aiEntity =
-            AllergyIntoleranceEntity.builder()
-                .cdwId(ai.cdwId())
-                .icn(patientIcn(ai.patient().isPresent() ? ai.patient().get() : null))
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(aiEntity);
-        break;
-      case "Condition":
-        DatamartCondition cond =
-            JacksonConfig.createMapper().readValue(file, DatamartCondition.class);
-        ConditionEntity condEntity =
-            ConditionEntity.builder()
-                .cdwId(cond.cdwId())
-                .icn(patientIcn(cond.patient()))
-                .category(cond.category().toString())
-                .clinicalStatus(cond.clinicalStatus().toString())
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(condEntity);
-        break;
-      case "DiagnosticReport":
-        DatamartDiagnosticReports dr =
-            JacksonConfig.createMapper().readValue(file, DatamartDiagnosticReports.class);
-        DiagnosticReportsEntity drEntity =
-            DiagnosticReportsEntity.builder()
-                .icn(dr.fullIcn())
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(drEntity);
-        break;
-      case "Immunization":
-        DatamartImmunization imm =
-            JacksonConfig.createMapper().readValue(file, DatamartImmunization.class);
-        ImmunizationEntity immEntity =
-            ImmunizationEntity.builder()
-                .cdwId(imm.cdwId())
-                .icn(patientIcn(imm.patient()))
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(immEntity);
-        break;
-      case "Medication":
-        DatamartMedication med =
-            JacksonConfig.createMapper().readValue(file, DatamartMedication.class);
-        MedicationEntity medEntity =
-            MedicationEntity.builder()
-                .cdwId(med.cdwId())
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(medEntity);
-        break;
-      case "MedicationOrder":
-        DatamartMedicationOrder medOrd =
-            JacksonConfig.createMapper().readValue(file, DatamartMedicationOrder.class);
-        MedicationOrderEntity moEntity =
-            MedicationOrderEntity.builder()
-                .cdwId(medOrd.cdwId())
-                .icn(patientIcn(medOrd.patient()))
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(moEntity);
-        break;
-      case "MedicationStatement":
-        DatamartMedicationStatement medSta =
-            JacksonConfig.createMapper().readValue(file, DatamartMedicationStatement.class);
-        MedicationStatementEntity msEntity =
-            MedicationStatementEntity.builder()
-                .cdwId(medSta.cdwId())
-                .icn(patientIcn(medSta.patient()))
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(msEntity);
-        break;
-      case "Observation":
-        DatamartObservation obs =
-            JacksonConfig.createMapper().readValue(file, DatamartObservation.class);
-        ObservationEntity obsEntity =
-            ObservationEntity.builder()
-                .cdwId(obs.cdwId())
-                .icn(obs.subject().isPresent() ? patientIcn(obs.subject().get()) : null)
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(obsEntity);
-        break;
-      case "Patient":
-        DatamartPatient pat = JacksonConfig.createMapper().readValue(file, DatamartPatient.class);
-        PatientSearchEntity patientSearchEntity =
-            PatientSearchEntity.builder()
-                .icn(pat.fullIcn())
-                .firstName(pat.firstName())
-                .lastName(pat.lastName())
-                .name(pat.name())
-                .birthDateTime(Instant.parse(pat.birthDateTime()))
-                .gender(pat.gender())
-                .build();
-        PatientEntity patEntity =
-            PatientEntity.builder()
-                .icn(pat.fullIcn())
-                .search(patientSearchEntity)
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(patEntity);
-        break;
-      case "Procedure":
-        DatamartProcedure proc =
-            JacksonConfig.createMapper().readValue(file, DatamartProcedure.class);
-        ProcedureEntity procEntity =
-            ProcedureEntity.builder()
-                .cdwId(proc.cdwId())
-                .icn(patientIcn(proc.patient()))
-                .payload(Files.readAllBytes(Paths.get(file.getPath())).toString())
-                .build();
-        entityManager.persist(procEntity);
-        break;
-      default:
-        throw new RuntimeException("Couldnt determine resource type for file: " + resourceName);
-    }
+  private static void insertByCondition(File file) {
+    DatamartCondition dm = JacksonConfig.createMapper().readValue(file, DatamartCondition.class);
+    ConditionEntity entity =
+        ConditionEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(patientIcn(dm.patient()))
+            .category(dm.category().toString())
+            .clinicalStatus(dm.clinicalStatus().toString())
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  // TODO Crosswalk Things
+  @SneakyThrows
+  private static void insertByDiagnosticReport(File file) {
+    DatamartDiagnosticReports dm =
+        JacksonConfig.createMapper().readValue(file, DatamartDiagnosticReports.class);
+    DiagnosticReportsEntity entity =
+        DiagnosticReportsEntity.builder().icn(dm.fullIcn()).payload(fileToString(file)).build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByImmunization(File file) {
+    DatamartImmunization dm =
+        JacksonConfig.createMapper().readValue(file, DatamartImmunization.class);
+    ImmunizationEntity entity =
+        ImmunizationEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(patientIcn(dm.patient()))
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByMedication(File file) {
+    DatamartMedication dm = JacksonConfig.createMapper().readValue(file, DatamartMedication.class);
+    MedicationEntity entity =
+        MedicationEntity.builder().cdwId(dm.cdwId()).payload(fileToString(file)).build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByMedictionOrder(File file) {
+    DatamartMedicationOrder dm =
+        JacksonConfig.createMapper().readValue(file, DatamartMedicationOrder.class);
+    MedicationOrderEntity entity =
+        MedicationOrderEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(patientIcn(dm.patient()))
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByMedicationStatement(File file) {
+    DatamartMedicationStatement dm =
+        JacksonConfig.createMapper().readValue(file, DatamartMedicationStatement.class);
+    MedicationStatementEntity entity =
+        MedicationStatementEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(patientIcn(dm.patient()))
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByObservation(File file) {
+    DatamartObservation dm =
+        JacksonConfig.createMapper().readValue(file, DatamartObservation.class);
+    ObservationEntity entity =
+        ObservationEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(dm.subject().isPresent() ? patientIcn(dm.subject().get()) : null)
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByPatient(File file) {
+    DatamartPatient dm = JacksonConfig.createMapper().readValue(file, DatamartPatient.class);
+    PatientSearchEntity patientSearchEntity =
+        PatientSearchEntity.builder()
+            .icn(dm.fullIcn())
+            .firstName(dm.firstName())
+            .lastName(dm.lastName())
+            .name(dm.name())
+            .birthDateTime(Instant.parse(dm.birthDateTime()))
+            .gender(dm.gender())
+            .build();
+    PatientEntity patEntity =
+        PatientEntity.builder()
+            .icn(dm.fullIcn())
+            .search(patientSearchEntity)
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(patEntity);
+    flushAndClear();
+  }
+
+  @SneakyThrows
+  private static void insertByProcedure(File file) {
+    DatamartProcedure dm = JacksonConfig.createMapper().readValue(file, DatamartProcedure.class);
+    ProcedureEntity entity =
+        ProcedureEntity.builder()
+            .cdwId(dm.cdwId())
+            .icn(patientIcn(dm.patient()))
+            .payload(fileToString(file))
+            .build();
+    entityManager.persist(entity);
+    flushAndClear();
   }
 
   /** Main. */
   public static void main(String[] args) {
-    /*if (args.length != 3) {
+    if (args.length != 3) {
       throw new RuntimeException("Arg Count Incorrect: " + args.length);
     }
     String resourceToSync = args[0];
     String directory = args[1];
-    entityManager = DatamartExporter.getH2(args[2]);*/
-    String resourceToSync = "AllergyIntolerance";
-    String directory = "/home/jhulbert/Downloads/";
-    entityManager = DatamartExporter.getH2("./src/test/resources/minimart");
+    entityManager = DatamartExporter.getH2(args[2]);
+
+    log.info("Syncing {} file in {} to {}", resourceToSync, directory, args[2]);
+
     File dmDirectory = new File(directory);
     if (dmDirectory.listFiles() == null) {
-      log.info("No files in directory {}", directory);
+      log.error("No files in directory {}", directory);
       throw new RuntimeException("No files found in directory: " + directory);
     }
+
     List<File> dmFiles = Arrays.stream(dmDirectory.listFiles()).collect(Collectors.toList());
-    dmFiles =
-        dmFiles
-            .stream()
-            .filter(
-                f ->
-                    StringUtils.containsIgnoreCase(
-                        f.getName(), determineFilePrefix(resourceToSync)))
-            .filter(f -> f.isFile())
-            .collect(Collectors.toList());
     entityManager.getTransaction().begin();
-    for (File file : dmFiles) {
-      insertByResourceType(resourceToSync, file);
-      entityManager.flush();
-      entityManager.clear();
+    switch (resourceToSync) {
+      case "AllergyIntolerance":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmAllInt.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByAllergyIntolerance);
+        break;
+      case "Condition":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmCon.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByCondition);
+        break;
+      case "DiagnosticReport":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmDiaRep.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByDiagnosticReport);
+        break;
+      case "Immunization":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmImm.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByImmunization);
+        break;
+      case "Medication":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmMed(?!Sta|Ord).*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByMedication);
+        break;
+      case "MedicationOrder":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmMedOrd.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByMedictionOrder);
+        break;
+      case "MedicationStatement":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmMedSta.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByMedicationStatement);
+        break;
+      case "Observation":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmObs.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByObservation);
+        break;
+      case "Patient":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmPat.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByPatient);
+        break;
+      case "Procedure":
+        dmFiles.stream()
+            .filter(f -> f.getName().matches("^dmPro.*json$"))
+            .filter(File::isFile)
+            .collect(Collectors.toList())
+            .forEach(MitreMinimartMaker::insertByProcedure);
+        break;
+      default:
+        throw new RuntimeException("Couldnt determine resource type for file: " + resourceToSync);
     }
+
     entityManager.getTransaction().commit();
     System.exit(0);
   }
 
+  private static void flushAndClear() {
+    entityManager.flush();
+    entityManager.clear();
+  }
+
   private static String patientIcn(DatamartReference dm) {
     return dm != null && dm.reference().isPresent() ? dm.reference().get() : null;
+  }
+
+  @SneakyThrows
+  private static String fileToString(File file) {
+    return new String(Files.readAllBytes(Paths.get(file.getPath())));
   }
 }
