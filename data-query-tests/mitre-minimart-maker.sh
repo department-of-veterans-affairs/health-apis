3b4e967ba8da417811099a7ff9c8c0ad83210987
jhulbert <joshua.hulbert@libertyits.com>
2019-08-27 08:27:03 -0400

    Add usage details to shell script

--- data-query-tests/mitre-minimart-maker.sh
+++ data-query-tests/mitre-minimart-maker.sh
@@ -5,11 +5,10 @@ DIRECTORY=$2
 RESOURCE_TYPE=$3
 
 cd $(dirname $0)
-PROJECT_VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v -E 'INFO|Download')
 
 pushToDatabase() {
-  [ -z "$DIRECTORY" ] && echo "Directory is a Required Param." && exit 1
-  [ -z "$RESOURCE_TYPE" ] && echo "Resource Type is a Required Param." && exit 1
+  [ -z "$DIRECTORY" ] && usage "Directory is a Required Param." && exit 1
+  [ -z "$RESOURCE_TYPE" ] && usage "Resource Type is a Required Param." && exit 1
   outputFile="./src/test/resources/minimart"
   [ -f "$outputFile" ] && rm -v "$ouputFile/*"
   mvn -f ../data-query test-compile && \
@@ -27,8 +26,8 @@ pushToDatabase() {
 }
 
 transformFhirToDatamart() {
-  [ -z "$DIRECTORY" ] && echo "Directory is a Required Param." && exit 1
-  [ -z "$RESOURCE_TYPE" ] && echo "Resource Type is a Required Param." && exit 1
+  [ -z "$DIRECTORY" ] && usage "Directory is a Required Param." && exit 1
+  [ -z "$RESOURCE_TYPE" ] && usage "Resource Type is a Required Param." && exit 1
   mvn -f ../data-query test-compile && \
     mvn -f ../data-query \
     -P'!standard' \
@@ -42,10 +41,28 @@ minimartDatabase() {
   java -jar ~/.m2/repository/com/h2database/h2/1.4.197/h2-1.4.197.jar -url jdbc:h2:./src/test/resources/minimart -user sa -password sa
 }
 
+usage() {
+cat <<EOF
+Commands:
+  transformToDatamart <directory-to-read-files-from> <resource-name>
+    Takes all files for the given resource in the directory and transforms them to datamart schema
+  pushToDb <directory-to-read-files-from> <resource-name>
+    Pushes all files for the given resource and directory to a local h2 repository
+  openDb
+    Open the database that was created by the pushToDb command
+
+Examples:
+  transformToDatamart "$(pwd)/data-query-tests/target" AllergyIntolerance
+  pushToDb "$(pwd)/data-query-tests/target/fhir-to-datamart" AllergyIntolerance
+
+$1
+EOF
+}
+
 case $COMMAND in
   pushToDb) pushToDatabase ;;
   transformToDatamart) transformFhirToDatamart ;;
-    #  mitreMinimart) transformFhirToDatamart && pushToDatabase ;;
   openDb) minimartDatabase ;;
-  *) echo "Invalid Command: $COMMAND" && exit 1 ;;
+  -h|--help) usage ;;
+  *) usage "Invalid Command: $COMMAND" && exit 1 ;;
 esac
