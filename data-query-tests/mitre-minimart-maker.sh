40886a1fe2475ad2fec8936cf5d3d70667de0ed1
jhulbert <joshua.hulbert@libertyits.com>
2019-08-28 14:45:29 -0400

    Applications run -- ids gets data -- dq cant find schema

--- data-query-tests/mitre-minimart-maker.sh
+++ data-query-tests/mitre-minimart-maker.sh
@@ -1,76 +1,106 @@
 #!/usr/bin/env bash
 
-COMMAND=$1
-
-cd $(dirname $0)
+REPO="$(cd $(dirname $0)/../../ && pwd)"
+IDS_H2="$REPO/health-apis-ids/ids/target/minimartIds"
+DQ_H2="$REPO/health-apis-data-query/data-query-tests/target/minimart"
 
 minimartIds() {
-  [ "${OPEN_DB:-false}" == 'true' ] && openDatabase '../../heath-apis-ids/ids/minimartIds'
+  [ "${OPEN_DB:-false}" == 'true' ] && openDatabase "$IDS_H2" && exit 0
   [ -z "$START$STOP" ] && usage "Missing options for minimartIds" && exit 1
-  [ "${START:-false}" == 'true' ] && startMinimartApp
-  [ "${STOP:-false}" == 'true' ] && stopMinimartApp
+  [ "${START:-false}" == 'true' ] \
+    && startMinimartApp ids "$REPO/health-apis-ids" "$IDS_H2" && exit 0
+  [ "${STOP:-false}" == 'true' ] && stopMinimartApp ids && exit 0
+  echo "Couldn't determine action to take..."
 }
 
 minimartDatabase() {
-  [ "${OPEN_DB:-false}" == 'true' ] \
-    && openDatabase './src/test/resources/minimart' \
-    || usage "Missing a valid option for minimartDb command..."
+  [ "${OPEN_DB:-false}" == 'true' ] && openDatabase "$DQ_H2" && exit 0
+  [ -z "$START$STOP" ] && usage "Missing options for minimartDb" && exit 1
+  [ "${START:-false}" == 'true' ] && [ "$DDL_AUTO" != 'create' ] \
+    && startMinimartApp data-query "$REPO/health-apis-data-query" "$DQ_H2" \
+    && exit 0
+  [ "${STOP:-false}" == 'true' ] && stopMinimartApp dq && exit 0
+  echo "Couldn't determine action to take..."
+}
+
+pidOf() {
+  local app=$1
+  jps -v | grep -F -- "-Dapp.name=$app" | cut -d ' ' -f 1
 }
 
 startMinimartApp() {
-  # Current dir should be the scripts location (./health-apis-data-query/data-query-tests)
-  cd ../../health-apis-ids/ids
-
-  # Find id services jar
-  local app=ids
+  local app=$1
+  local where=$2
   local pid=$(pidOf $app)
-  [ -n "$pid" ] && echo "$app appears to already be running ($pid)" && cd - && exit 1
+  [ -n "$pid" ] && echo "$app appears to already be running ($pid)" && return
+  echo "Starting $app"
+  [ ! -d "$where" ] && echo "$where does not exist" && exit 1
+  cd $where/$app
   local jar=$(find target -maxdepth 1 -name "$app-*.jar" | grep -v 'tests')
   [ -z "$jar" ] && echo "Cannot find $app application jar" && exit 1
   local options="-Dapp.name=$app"
 
-  # Startup a new or existing minimart database and load it with goodies
+  local pathSeparator=':'
   [ "$(uname)" != "Darwin" ] && [ "$(uname)" != "Linux" ] && echo "Add support for your operating system" && exit 1
-  echo "Starting ids for minimart..."
-  options+=" -cp $(readlink -f $jar):$(readlink -f ~/.m2/repository/com/h2database/h2/1.4.197/h2-1.4.197.jar)"
-  options+=" -Dspring.jpa.generate-ddl=true"
+  echo "Using local H2 database for $app..."
+  options+=" -cp $(readlink -f $jar)${pathSeparator}$(readlink -f ~/.m2/repository/com/h2database/h2/1.4.197/h2-1.4.197.jar)"
+  if [ "$app" == 'data-query' ]
+  then
+    # application-dev.properties
+    options+=" -Didentityservice.url=http://localhost:8089"
+    options+=" -Dmranderson.url=http://localhost:8088"
+    options+=" -Dargonaut.url=http://localhost:8090"
+    options+=" -Dwell-known.capabilities=context-standalone-patient,launch-ehr,permission-offline,permission-patient"
+    options+=" -Dwell-known.response-type-supported=code,refresh_token"
+    options+=" -Dwell-known.scopes-supported=patient/DiagnosticReport.read,patient/Patient.read,offline_access"
+    options+=" -Dconformance.statement-type=patient"
+    options+=" -Dconformance.contact.name=jhulbert"
+    options+=" -Dconformance.contact.email=joshua.hulbert@libertyits.com"
+    options+=" -Dconformance.security.token-endpoint=http://fake.com/token"
+    options+=" -Dconformance.security.authorize-endpoint=http://fake.com/authorize"
+    options+=" -Dhealth-check.read-frequency-ms=300000"
+  fi
+  options+=" -Dspring.jpa.generate-ddl=false"
   options+=" -Dspring.jpa.hibernate.ddl-auto=$DDL_AUTO"
-  options+=" -Dspring.jpa.hibernate.globally_quoted_identifiers=true"
+  options+=" -Dspring.jpa.hibernate.globally_quoted_identifiers=false"
   options+=" -Dspring.datasource.driver-class-name=org.h2.Driver"
-  options+=" -Dspring.datasource.url=jdbc:h2:file:$(pwd)/minimartIds"
+  options+=" -Dspring.datasource.url=jdbc:h2:file:$3"
   options+=" -Dspring.datasource.username=sa"
   options+=" -Dspring.datasource.password=sa"
   java ${options} org.springframework.boot.loader.PropertiesLauncher &
 }
 
 stopMinimartApp() {
-  ../src/scripts/dev-app.sh -i stop
+  [ "$1" == 'ids' ] && OPTION="-i"
+  [ "$1" == 'dq' ] && OPTION='-d'
+  $REPO/health-apis-data-query/src/scripts/dev-app.sh "$OPTION" stop
 }
 
 pushToDatabase() {
+  local workingDir="$REPO/health-apis-data-query"
   [ -z "$DIRECTORY" ] && usage "Directory is a Required Param." && exit 1
   [ -z "$RESOURCE_TYPE" ] && usage "Resource Type is a Required Param." && exit 1
-  outputFile="./src/test/resources/minimart"
   [ -f "$outputFile" ] && rm -v "$ouputFile/*"
-  mvn -f ../data-query test-compile && \
-    mvn -f ../data-query \
+  mvn -f "$workingDir/data-query" test-compile && \
+    mvn -f "$workingDir/data-query" \
     -P'!standard' \
     -Pmitre-minimart-maker \
     exec:java@pushToDatabase \
     generate-resources \
     -DresourceType="$RESOURCE_TYPE" \
     -DinputDirectory="$DIRECTORY" \
-    -DoutputFile="$outputFile" \
+    -DoutputFile="$DQ_H2" \
     -Dorg.jboss.logging.provider=jdk \
     -Djava.util.logging.config.file=nope
 
 }
 
 transformFhirToDatamart() {
+  local workingDir="$REPO/health-apis-data-query"
   [ -z "$DIRECTORY" ] && usage "Directory is a Required Option." && exit 1
   [ -z "$RESOURCE_TYPE" ] && usage "Resource Type is a Required Option." && exit 1
-  mvn -f ../data-query test-compile && \
-    mvn -f ../data-query \
+  mvn -f "$workingDir/data-query" test-compile && \
+    mvn -f "$workingDir/data-query" \
     -P'!standard' \
     -Pmitre-minimart-maker \
     generate-resources \
@@ -84,6 +114,7 @@ openDatabase() {
 
 usage() {
 cat <<EOF
+---
 Commands:
   minimartIds <create|start|stop>
     Creates, starts, or stops the local ids
@@ -91,9 +122,9 @@ Commands:
     Takes all files for the given resource in the directory and transforms them to datamart schema
   pushToMinimartDb <directory-to-read-files-from> <resource-name>
     Pushes all files for the given resource and directory to a local h2 repository
-  openMinimartDb
-    Open the database that was created by the pushToDb command
-
+  minimartDb <create|start|stop>
+    Creates, starts, or stops the local data-query minimart app
+---
 Options:
   -s|--start) Can be used with minimartIds command to start local minimartIds (db must first be created)
   -k|--stop) Can be used with minimartIds command to stop local minimartIds
@@ -102,36 +133,40 @@ Options:
   -r|--resource) Use to specify the resource to transform or push to db
   -o|--open) Open the database from the given command
   -h|--help) I need an adult!!!
-
+---
 Examples:
   minimartIds --create|--start|--stop|--open
   transformToDatamart -d "$(pwd)/data-query-tests/target" -r AllergyIntolerance
   pushToMinimartDb -d "$(pwd)/data-query-tests/target/fhir-to-datamart" -r AllergyIntolerance
   minimartDb --create|--start|--stop|--open
-
+---
 $1
 EOF
 }
 
 ARGS=$(getopt -n $(basename ${0}) \
-    -l "help,start,stop,directory:,resource:,create:,open" \
-    -o "hskd:r:c:o" -- "$@")
+    -l "help,start,stop,directory:,resource:,create,open" \
+    -o "hskd:r:co" -- "$@")
 [ $? != 0 ] && usage
 eval set -- "$ARGS"
 while true
 do
   case "$1" in
-    -s|--start) START=true && DDL_AUTO="validate";;
+    -s|--start) START=true && DDL_AUTO="none";;
     -k|--stop) STOP=true;;
     -c|--create) START=true && DDL_AUTO="create";;
     -d|--directory) DIRECTORY="$2";;
     -r|--resource) RESOURCE_TYPE="$2";;
     -o|--open) OPEN_DB=true;;
-    -h|--help) usage;;
+    -h|--help) usage && exit 0;;
     --) shift;break;;
   esac
   shift;
 done
+
+COMMAND=$1
+
+echo "Working from directory: $REPO"
 
 case $COMMAND in
   minimartIds) minimartIds;;
