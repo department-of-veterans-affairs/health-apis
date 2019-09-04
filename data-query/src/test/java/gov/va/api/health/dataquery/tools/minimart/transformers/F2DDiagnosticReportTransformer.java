895d8a7d6e2e61720d866a876f26ecbfc8f43f7a
jhulbert <joshua.hulbert@libertyits.com>
2019-09-04 09:25:23 -0400

    publicIds to cdwIds

--- data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/transformers/F2DDiagnosticReportTransformer.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/transformers/F2DDiagnosticReportTransformer.java
@@ -5,13 +5,15 @@ import static org.codehaus.groovy.runtime.InvokerHelper.asList;
 import gov.va.api.health.argonaut.api.resources.DiagnosticReport;
 import gov.va.api.health.dataquery.service.controller.Transformers;
 import gov.va.api.health.dataquery.service.controller.diagnosticreport.DatamartDiagnosticReports;
+import gov.va.api.health.dataquery.tools.minimart.RevealSecretIdentity;
 import java.util.List;
 
 public class F2DDiagnosticReportTransformer {
 
   public DatamartDiagnosticReports fhirToDatamart(DiagnosticReport diagnosticReport) {
     return DatamartDiagnosticReports.builder()
-        .fullIcn(splitReference(diagnosticReport.subject().reference(), "Patient"))
+        .fullIcn(
+            RevealSecretIdentity.unmask(splitReference(diagnosticReport.subject().reference())))
         .patientName(diagnosticReport.subject().display())
         .reports(reports(diagnosticReport))
         .build();
@@ -22,12 +24,13 @@ public class F2DDiagnosticReportTransformer {
     return Transformers.emptyToNull(
         asList(
             DatamartDiagnosticReports.DiagnosticReport.builder()
-                .identifier(diagnosticReport.id())
+                .identifier(RevealSecretIdentity.unmask(diagnosticReport.id()))
                 .effectiveDateTime(diagnosticReport.effectiveDateTime())
                 .issuedDateTime(diagnosticReport.issued())
                 .accessionInstitutionSid(
                     diagnosticReport.performer() != null
-                        ? splitReference(diagnosticReport.performer().reference(), "Organization")
+                        ? RevealSecretIdentity.unmask(
+                            splitReference(diagnosticReport.performer().reference()))
                         : null)
                 .accessionInstitutionName(
                     diagnosticReport.performer() != null
@@ -36,7 +39,7 @@ public class F2DDiagnosticReportTransformer {
                 .build()));
   }
 
-  public String splitReference(String reference, String resource) {
+  public String splitReference(String reference) {
     String[] splitRef = reference.split("/");
     return splitRef[splitRef.length - 1];
   }
