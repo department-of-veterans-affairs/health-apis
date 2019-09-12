deb9e0077ab373571b5ecefe5b14c911652da6bc
Ian LaFlamme <ian.laflamme@libertyits.com>
2019-09-12 12:39:15 -0400

    f2d ms transformer

--- data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/MitreMinimartMaker.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/tools/minimart/MitreMinimartMaker.java
@@ -1,6 +1,7 @@
 package gov.va.api.health.dataquery.tools.minimart;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import gov.va.api.health.argonaut.api.resources.MedicationStatement;
 import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.dataquery.service.controller.allergyintolerance.AllergyIntoleranceEntity;
 import gov.va.api.health.dataquery.service.controller.allergyintolerance.DatamartAllergyIntolerance;
@@ -49,7 +50,8 @@ public class MitreMinimartMaker {
           ConditionEntity.class,
           DiagnosticReportsEntity.class,
           DiagnosticReportCrossEntity.class,
-          ImmunizationEntity.class
+          ImmunizationEntity.class,
+          MedicationStatement.class
           //
           );
 
