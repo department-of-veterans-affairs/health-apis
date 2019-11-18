5f85296946dded6de748e8314c8ea1833edd6927
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-11-18 12:51:09 -0500

    .

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/location/DatamartLocation.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/location/DatamartLocation.java
@@ -1,11 +1,6 @@
 package gov.va.api.health.dataquery.service.controller.location;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import gov.va.api.health.dataquery.service.controller.datamart.DatamartReference;
 import gov.va.api.health.dataquery.service.controller.datamart.HasReplaceableId;
-import java.time.Instant;
-import java.util.Optional;
-import javax.validation.constraints.Min;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,24 +18,24 @@ public final class DatamartLocation implements HasReplaceableId {
 
   private String cdwId;
 
-//  private Optional<DatamartReference> performer;
-//
-//  /** Lazy initialization with empty. */
-//  public Optional<DatamartReference> encounter() {
-//    if (encounter == null) {
-//      encounter = Optional.empty();
-//    }
-//    return encounter;
-//  }
-//
-//  @Data
-//  @Builder
-//  @NoArgsConstructor(access = AccessLevel.PRIVATE)
-//  @AllArgsConstructor(access = AccessLevel.PRIVATE)
-//  public static class VaccineCode {
-//
-//    private String text;
-//
-//    private String code;
-//  }
+  //  private Optional<DatamartReference> performer;
+  //
+  //  /** Lazy initialization with empty. */
+  //  public Optional<DatamartReference> encounter() {
+  //    if (encounter == null) {
+  //      encounter = Optional.empty();
+  //    }
+  //    return encounter;
+  //  }
+  //
+  //  @Data
+  //  @Builder
+  //  @NoArgsConstructor(access = AccessLevel.PRIVATE)
+  //  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  //  public static class VaccineCode {
+  //
+  //    private String text;
+  //
+  //    private String code;
+  //  }
 }
