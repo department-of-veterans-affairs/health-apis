c0873a72a9df2fb9046ab36e56b0f51da96005a2
jjennings <joshua.jennings@libertyits.com>
2019-11-27 16:13:23 -0500

    1% improvement on coverage. More to come

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/practitioner/DatamartPractitionerSamples.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/practitioner/DatamartPractitionerSamples.java
@@ -1,15 +1,23 @@
 package gov.va.api.health.dataquery.service.controller.practitioner;
 
+import gov.va.api.health.dataquery.service.controller.datamart.DatamartCoding;
+import gov.va.api.health.dataquery.service.controller.datamart.DatamartReference;
 import gov.va.api.health.dstu2.api.bundle.AbstractBundle.BundleType;
 import gov.va.api.health.dstu2.api.bundle.AbstractEntry.Search;
 import gov.va.api.health.dstu2.api.bundle.AbstractEntry.SearchMode;
 import gov.va.api.health.dstu2.api.bundle.BundleLink;
 import gov.va.api.health.dstu2.api.bundle.BundleLink.LinkRelation;
+import gov.va.api.health.dstu2.api.datatypes.Address;
+import gov.va.api.health.dstu2.api.datatypes.CodeableConcept;
+import gov.va.api.health.dstu2.api.datatypes.Coding;
 import gov.va.api.health.dstu2.api.datatypes.ContactPoint;
 import gov.va.api.health.dstu2.api.datatypes.HumanName;
+import gov.va.api.health.dstu2.api.elements.Reference;
 import gov.va.api.health.dstu2.api.resources.Practitioner;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -38,9 +46,33 @@ public class DatamartPractitionerSamples {
                       .postalCode("99501")
                       .build()))
           .name(DatamartPractitioner.Name.builder().family("Joe").given("Johnson").build())
-          // .birthDate(Optional.of(LocalDate.parse("1970-11-14T00:00:00Z")))
+          .birthDate(Optional.of(LocalDate.parse("1970-11-14")))
           .gender(DatamartPractitioner.Gender.male)
           .npi(Optional.of("1234567"))
+          .practitionerRole(
+              Optional.of(
+                  DatamartPractitioner.PractitionerRole.builder()
+                      .healthCareService(Optional.of("medical"))
+                      .location(
+                          Collections.singletonList(
+                              DatamartReference.builder()
+                                  .display(Optional.of("test"))
+                                  .type(Optional.of("Location"))
+                                  .build()))
+                      .managingOrganization(
+                          Optional.of(
+                              DatamartReference.builder()
+                                  .display(Optional.of("test"))
+                                  .type(Optional.of("Organization"))
+                                  .build()))
+                      .role(
+                          Optional.of(
+                              DatamartCoding.builder()
+                                  .system(Optional.of("test"))
+                                  .display(Optional.of("test"))
+                                  .code(Optional.of("test"))
+                                  .build()))
+                      .build()))
           .telecom(
               List.of(
                   DatamartPractitioner.Telecom.builder()
@@ -93,10 +125,10 @@ public class DatamartPractitionerSamples {
             .active(true)
             .name(HumanName.builder().family(List.of("Joe")).given(List.of("Johnson")).build())
             .gender(Practitioner.Gender.male)
-            // .birthDate("1970-11-14T00:00:00Z")
+            .birthDate("1970-11-14")
             .address(
                 List.of(
-                    gov.va.api.health.dstu2.api.datatypes.Address.builder()
+                    Address.builder()
                         .line(List.of("111 MacGyver Viaduct"))
                         .city("Anchorage")
                         .state("Alaska")
@@ -108,6 +140,26 @@ public class DatamartPractitionerSamples {
                         .use(ContactPoint.ContactPointUse.mobile)
                         .value("123-456-1234")
                         .system(ContactPoint.ContactPointSystem.phone)
+                        .build()))
+            .practitionerRole(
+                Collections.singletonList(
+                    Practitioner.PractitionerRole.builder()
+                        .location(
+                            Collections.singletonList(Reference.builder().display("test").build()))
+                        .healthcareService(
+                            Collections.singletonList(
+                                Reference.builder().display("medical").build()))
+                        .managingOrganization(Reference.builder().display("test").build())
+                        .role(
+                            CodeableConcept.builder()
+                                .coding(
+                                    Collections.singletonList(
+                                        Coding.builder()
+                                            .code("test")
+                                            .display("test")
+                                            .system("test")
+                                            .build()))
+                                .build())
                         .build()))
             .build();
       }
