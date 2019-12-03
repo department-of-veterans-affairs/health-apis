3357c30f25cfbbf85b8b3916b4992ceba1818058
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-12-03 15:27:00 -0500

    .

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/location/DatamartLocationTransformer.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/location/DatamartLocationTransformer.java
@@ -1,43 +1,30 @@
 package gov.va.api.health.dataquery.service.controller.location;
 
-import static java.util.Arrays.asList;
 import static gov.va.api.health.dataquery.service.controller.Transformers.allBlank;
-
+import static gov.va.api.health.dataquery.service.controller.Transformers.asReference;
+import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.StringUtils.isBlank;
 
+import gov.va.api.health.dataquery.service.controller.EnumSearcher;
 import gov.va.api.health.dstu2.api.datatypes.Address;
+import gov.va.api.health.dstu2.api.datatypes.CodeableConcept;
+import gov.va.api.health.dstu2.api.datatypes.Coding;
+import gov.va.api.health.dstu2.api.datatypes.ContactPoint;
 import gov.va.api.health.dstu2.api.resources.Location;
+import java.util.List;
+import java.util.Optional;
 import lombok.Builder;
 
 @Builder
 final class DatamartLocationTransformer {
   private final DatamartLocation datamart;
 
-  /** Convert the datamart structure to FHIR compliant structure. */
-  public Location toFhir() {
-    return Location.builder()
-        .resourceType("Location")
-        .id(datamart.cdwId())
-        .address(address(datamart.address()))
-        .build();
-
-    //    .description(source.getDescription())
-    //    .managingOrganization(reference(source.getManagingOrganization()))
-    //    .mode(mode(source.getMode()))
-    //    .name(source.getName())
-    //    .physicalType(locationPhysicalType(source.getPhysicalType()))
-    //    .status(status(source.getStatus()))
-    //    .telecom(telecoms(source.getTelecoms()))
-    //    .type(locationType(source.getType()))
-  }
-
-  private Address address(DatamartLocation.Address address) {
+  private static Address address(DatamartLocation.Address address) {
     if (address == null) {
       return null;
     }
     if (isBlank(address.line1())
         || allBlank(address.city(), address.state(), address.postalCode())) {
-
       return null;
     }
     return Address.builder()
@@ -45,6 +32,69 @@ final class DatamartLocationTransformer {
         .city(address.city())
         .state(address.state())
         .postalCode(address.postalCode())
+        .build();
+  }
+
+  private static CodeableConcept physicalType(Optional<String> maybePhysType) {
+    if (maybePhysType.isEmpty()) {
+      return null;
+    }
+
+    String physType = maybePhysType.get();
+    if (isBlank(physType)) {
+      return null;
+    }
+
+    return CodeableConcept.builder()
+        .coding(asList(Coding.builder().display(physType).build()))
+        .build();
+  }
+
+  private static Location.Status status(DatamartLocation.Status status) {
+    if (status == null) {
+      return null;
+    }
+    return EnumSearcher.of(Location.Status.class).find(status.toString());
+  }
+
+  private static List<ContactPoint> telecoms(String telecom) {
+    if (isBlank(telecom)) {
+      return null;
+    }
+    return asList(
+        ContactPoint.builder()
+            .system(ContactPoint.ContactPointSystem.phone)
+            .value(telecom)
+            .build());
+  }
+
+  private static CodeableConcept type(Optional<String> maybeType) {
+    if (maybeType.isEmpty()) {
+      return null;
+    }
+
+    String type = maybeType.get();
+    if (isBlank(type)) {
+      return null;
+    }
+
+    return CodeableConcept.builder().coding(asList(Coding.builder().display(type).build())).build();
+  }
+
+  /** Convert the datamart structure to FHIR compliant structure. */
+  public Location toFhir() {
+    return Location.builder()
+        .resourceType("Location")
+        .id(datamart.cdwId())
+        .address(address(datamart.address()))
+        .description(datamart.description().orElse(null))
+        .managingOrganization(asReference(datamart.managingOrganization()))
+        .mode(Location.Mode.instance)
+        .name(datamart.name())
+        .physicalType(physicalType(datamart.physicalType()))
+        .status(status(datamart.status()))
+        .telecom(telecoms(datamart.telecom()))
+        .type(type(datamart.type()))
         .build();
   }
 }
