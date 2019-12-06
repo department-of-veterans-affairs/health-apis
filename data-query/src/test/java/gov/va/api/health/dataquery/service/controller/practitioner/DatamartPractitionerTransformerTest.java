c61fe0d633d7a39ba6215abded17f55bb101a929
jjennings <joshua.jennings@libertyits.com>
2019-12-06 10:03:06 -0500

    Adding some transformer tests

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/practitioner/DatamartPractitionerTransformerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/practitioner/DatamartPractitionerTransformerTest.java
@@ -1,8 +1,20 @@
 package gov.va.api.health.dataquery.service.controller.practitioner;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LOCAL_DATE;
 
+import gov.va.api.health.dataquery.service.controller.EnumSearcher;
+import gov.va.api.health.dataquery.service.controller.practitioner.DatamartPractitioner;
+import gov.va.api.health.dataquery.service.controller.practitioner.DatamartPractitionerTransformer;
+import gov.va.api.health.dstu2.api.datatypes.Address;
+import gov.va.api.health.dstu2.api.resources.Location;
+import gov.va.api.health.dstu2.api.resources.Practitioner;
+import org.apache.tomcat.jni.Local;
 import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 public class DatamartPractitionerTransformerTest {
 
@@ -11,6 +23,38 @@ public class DatamartPractitionerTransformerTest {
     assertThat(tx(DatamartPractitionerSamples.Datamart.create().practitioner()).toFhir())
         .isEqualTo(DatamartPractitionerSamples.Datamart.Fhir.create().practitioner());
   }
+
+
+  @Test
+  public void address() {
+    assertThat(DatamartPractitionerTransformer.address(null)).isNull();
+
+    assertThat(
+            DatamartPractitionerTransformer.address(
+                    DatamartPractitioner.Address.builder().city(" ").state(" ").postalCode(" ").build()))
+            .isNull();
+    assertThat(
+            DatamartPractitionerTransformer.address(
+                    DatamartPractitioner.Address.builder()
+                            .line1("w")
+                            .city("x")
+                            .state("y")
+                            .postalCode("z")
+                            .build()))
+            .isEqualTo(
+                    Address.builder().line(asList("w")).city("x").state("y").postalCode("z").build());
+  }
+
+  @Test
+  public void gender() {
+    DatamartPractitionerTransformer transformer = DatamartPractitionerTransformer.builder().build();
+    assertThat(transformer.gender(null)).isNull();
+    assertThat(transformer.gender(DatamartPractitioner.Gender.male))
+            .isEqualTo(Practitioner.Gender.male);
+    assertThat(transformer.gender(DatamartPractitioner.Gender.female))
+            .isEqualTo(Practitioner.Gender.female);
+  }
+  
 
   DatamartPractitionerTransformer tx(DatamartPractitioner dm) {
     return DatamartPractitionerTransformer.builder().datamart(dm).build();
