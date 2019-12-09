de2012abae0ac401cb4548f1dea21b98198ab0aa
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2019-12-09 11:54:24 -0500

    .

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/Dstu2Validator.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/Dstu2Validator.java
@@ -3,7 +3,10 @@ package gov.va.api.health.dataquery.service.controller;
 import gov.va.api.health.dstu2.api.bundle.AbstractBundle;
 import gov.va.api.health.dstu2.api.datatypes.CodeableConcept;
 import gov.va.api.health.dstu2.api.elements.Narrative;
+import gov.va.api.health.dstu2.api.elements.Narrative.NarrativeStatus;
 import gov.va.api.health.dstu2.api.resources.OperationOutcome;
+import gov.va.api.health.dstu2.api.resources.OperationOutcome.Issue;
+import gov.va.api.health.dstu2.api.resources.OperationOutcome.Issue.IssueSeverity;
 import java.util.Collections;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
@@ -24,13 +27,13 @@ public class Dstu2Validator {
         .id("allok")
         .text(
             Narrative.builder()
-                .status(Narrative.NarrativeStatus.additional)
+                .status(NarrativeStatus.additional)
                 .div("<div xmlns=\"http://www.w3.org/1999/xhtml\"><p>ALL OK</p></div>")
                 .build())
         .issue(
             Collections.singletonList(
-                OperationOutcome.Issue.builder()
-                    .severity(OperationOutcome.Issue.IssueSeverity.information)
+                Issue.builder()
+                    .severity(IssueSeverity.information)
                     .code("informational")
                     .details(CodeableConcept.builder().text("ALL OK").build())
                     .build()))
