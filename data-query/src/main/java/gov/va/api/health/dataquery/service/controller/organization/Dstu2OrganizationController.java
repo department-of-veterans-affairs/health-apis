2686570fc1db977afe216978b262209c5877f7af
Jen <jennifer.nguy@libertyits.com>
2020-01-06 15:52:24 -0500

    Majig fix

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/organization/Dstu2OrganizationController.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/organization/Dstu2OrganizationController.java
@@ -4,11 +4,11 @@ import static gov.va.api.health.dataquery.service.controller.Dstu2Transformers.f
 import static gov.va.api.health.dataquery.service.controller.Dstu2Transformers.hasPayload;
 import static java.util.Collections.emptyList;
 
-import gov.va.api.health.dataquery.service.controller.AbstractIncludesIcnMajig;
 import gov.va.api.health.dataquery.service.controller.CountParameter;
 import gov.va.api.health.dataquery.service.controller.Dstu2Bundler;
 import gov.va.api.health.dataquery.service.controller.Dstu2Bundler.BundleContext;
 import gov.va.api.health.dataquery.service.controller.Dstu2Validator;
+import gov.va.api.health.dataquery.service.controller.IncludesIcnMajig;
 import gov.va.api.health.dataquery.service.controller.PageLinks;
 import gov.va.api.health.dataquery.service.controller.PageLinks.LinkConfig;
 import gov.va.api.health.dataquery.service.controller.Parameters;
@@ -122,7 +122,7 @@ public class Dstu2OrganizationController {
     headers = {"raw=true"}
   )
   public String readRaw(@PathVariable("publicId") String publicId, HttpServletResponse response) {
-    AbstractIncludesIcnMajig.addHeaderForNoPatients(response);
+    IncludesIcnMajig.addHeaderForNoPatients(response);
     return datamart.readRaw(publicId).payload();
   }
 
