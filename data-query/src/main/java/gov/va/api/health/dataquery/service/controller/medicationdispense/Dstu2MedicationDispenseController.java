2b1419784b3e6ed236d0aea51d315d0f81dd5f5a
David Lennon <david.lennon@libertyits.com>
2020-01-07 08:36:48 -0500

    Fixed tests

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medicationdispense/Dstu2MedicationDispenseController.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medicationdispense/Dstu2MedicationDispenseController.java
@@ -2,19 +2,18 @@ package gov.va.api.health.dataquery.service.controller.medicationdispense;
 
 import gov.va.api.health.dataquery.service.controller.CountParameter;
 import gov.va.api.health.dataquery.service.controller.Dstu2Validator;
+import gov.va.api.health.dataquery.service.controller.ResourceExceptions;
 import gov.va.api.health.dstu2.api.resources.MedicationDispense;
 import gov.va.api.health.dstu2.api.resources.OperationOutcome;
 import javax.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @SuppressWarnings("WeakerAccess")
@@ -28,61 +27,55 @@ public class Dstu2MedicationDispenseController {
 
   /** Reading by id. */
   @GetMapping(value = {"/{publicId}"})
-  @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
   public MedicationDispense read(@PathVariable("publicId") String publicId) {
-    return null;
+    throw new ResourceExceptions.NotImplemented("not-implemented");
   }
 
   /** Searching by _id. */
   @GetMapping(params = {"_id"})
-  @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
   public MedicationDispense.Bundle searchById(
       @RequestParam("_id") String id,
       @RequestParam(value = "page", defaultValue = "1") @Min(1) int page,
       @CountParameter @Min(0) int count) {
-    return null;
+    throw new ResourceExceptions.NotImplemented("not-implemented");
   }
 
   /** Searching by identifier. */
   @GetMapping(params = {"identifier"})
-  @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
   public MedicationDispense.Bundle searchByIdentifier(
       @RequestParam("identifier") String id,
       @RequestParam(value = "page", defaultValue = "1") @Min(1) int page,
       @CountParameter @Min(0) int count) {
-    return null;
+    throw new ResourceExceptions.NotImplemented("not-implemented");
   }
 
   /** Searching by patient. */
   @GetMapping(params = {"patient"})
-  @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
   public MedicationDispense.Bundle searchByPatient(
       @RequestParam("patient") String patient,
       @RequestParam(value = "page", defaultValue = "1") @Min(1) int page,
       @CountParameter @Min(0) int count) {
-    return null;
+    throw new ResourceExceptions.NotImplemented("not-implemented");
   }
 
   /** Searching by patient and status. */
   @GetMapping(params = {"patient", "status"})
-  @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
   public MedicationDispense.Bundle searchByPatientAndStatus(
       @RequestParam("patient") String patient,
       @RequestParam("status") String status,
       @RequestParam(value = "page", defaultValue = "1") @Min(1) int page,
       @CountParameter @Min(0) int count) {
-    return null;
+    throw new ResourceExceptions.NotImplemented("not-implemented");
   }
 
   /** Searching by patient and type. */
   @GetMapping(params = {"patient", "type"})
-  @ResponseStatus(HttpStatus.NOT_IMPLEMENTED)
   public MedicationDispense.Bundle searchByPatientAndType(
       @RequestParam("patient") String patient,
       @RequestParam("type") String type,
       @RequestParam(value = "page", defaultValue = "1") @Min(1) int page,
       @CountParameter @Min(0) int count) {
-    return null;
+    throw new ResourceExceptions.NotImplemented("not-implemented");
   }
 
   /** Validation endpoint. */
