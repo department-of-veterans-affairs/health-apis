ab87cb1c0424918a2aee7c61aa9f8e548c2abe93
Jen <jennifer.nguy@libertyits.com>
2020-01-09 15:39:04 -0500

    Added tests

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/organization/Dstu2OrganizationTransformerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/organization/Dstu2OrganizationTransformerTest.java
@@ -3,8 +3,10 @@ package gov.va.api.health.dataquery.service.controller.organization;
 import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import gov.va.api.health.autoconfig.configuration.JacksonConfig;
 import gov.va.api.health.dstu2.api.datatypes.Address;
 import gov.va.api.health.dstu2.api.datatypes.ContactPoint;
+import lombok.SneakyThrows;
 import org.junit.Test;
 
 public class Dstu2OrganizationTransformerTest {
@@ -40,6 +42,22 @@ public class Dstu2OrganizationTransformerTest {
                     .build()));
   }
 
+  @SneakyThrows
+  String json(Object o) {
+    return JacksonConfig.createMapper().writerWithDefaultPrettyPrinter().writeValueAsString(o);
+  }
+
+  @Test
+  public void organization() {
+    assertThat(
+            json(
+                Dstu2OrganizationTransformer.builder()
+                    .datamart(OrganizationSamples.Datamart.create().organization())
+                    .build()
+                    .toFhir()))
+        .isEqualTo(json(OrganizationSamples.Dstu2.create().organization()));
+  }
+
   @Test
   public void telecom() {
     assertThat(Dstu2OrganizationTransformer.telecom(null)).isNull();
@@ -54,5 +72,13 @@ public class Dstu2OrganizationTransformerTest {
                 .system(ContactPoint.ContactPointSystem.phone)
                 .value("abc")
                 .build());
+  }
+
+  @Test
+  public void telecomSystem() {
+    assertThat(Dstu2OrganizationTransformer.telecomSystem(null)).isNull();
+    assertThat(
+            Dstu2OrganizationTransformer.telecomSystem(DatamartOrganization.Telecom.System.phone))
+        .isEqualTo(ContactPoint.ContactPointSystem.phone);
   }
 }
