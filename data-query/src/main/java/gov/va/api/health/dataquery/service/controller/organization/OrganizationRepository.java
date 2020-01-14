fac172f83046185e635ecc15e18f728a8109067b
Jen <jennifer.nguy@libertyits.com>
2020-01-14 12:07:52 -0500

    First goaround

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/organization/OrganizationRepository.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/organization/OrganizationRepository.java
@@ -1,8 +1,58 @@
 package gov.va.api.health.dataquery.service.controller.organization;
 
 import gov.va.api.health.autoconfig.logging.Loggable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import lombok.Builder;
+import lombok.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 @Loggable
 public interface OrganizationRepository
-    extends PagingAndSortingRepository<OrganizationEntity, String> {}
+    extends PagingAndSortingRepository<OrganizationEntity, String>,
+        JpaSpecificationExecutor<OrganizationEntity> {
+  // Page<OrganizationEntity> findByName(String name, PageRequest page);
+
+  Page<OrganizationEntity> findByName(String name, Pageable pageable);
+
+  @Value
+  @Builder
+  class AddressSpecification implements Specification<OrganizationEntity> {
+    String street;
+
+    String city;
+
+    String state;
+
+    String postalCode;
+
+    @Override
+    public Predicate toPredicate(
+        Root<OrganizationEntity> root,
+        CriteriaQuery<?> criteriaQuery,
+        CriteriaBuilder criteriaBuilder) {
+      List<Predicate> predicates = new ArrayList<>(4);
+      if (street != null) {
+        predicates.add(criteriaBuilder.equal(root.get("street"), street()));
+      }
+      if (city != null) {
+        predicates.add(criteriaBuilder.equal(root.get("city"), city()));
+      }
+      if (state != null) {
+        predicates.add(criteriaBuilder.equal(root.get("state"), state()));
+      }
+      if (postalCode != null) {
+        predicates.add(criteriaBuilder.equal(root.get("postalCode"), postalCode()));
+      }
+      return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+    }
+  }
+}
---
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/organization/Stu3OrganizationTransformer.java
@@ -0,0 +1,111 @@
+package gov.va.api.health.dataquery.service.controller.organization;
+
+import static gov.va.api.health.dataquery.service.controller.Transformers.allBlank;
+import static gov.va.api.health.dataquery.service.controller.Transformers.convert;
+import static gov.va.api.health.dataquery.service.controller.Transformers.emptyToNull;
+import static gov.va.api.health.dataquery.service.controller.Transformers.isBlank;
+import static java.util.Arrays.asList;
+import static org.apache.commons.lang3.StringUtils.trimToEmpty;
+import static org.apache.commons.lang3.StringUtils.trimToNull;
+
+import gov.va.api.health.dataquery.service.controller.EnumSearcher;
+import gov.va.api.health.dataquery.service.controller.datamart.DatamartCoding;
+import gov.va.api.health.stu3.api.datatypes.CodeableConcept;
+import gov.va.api.health.stu3.api.datatypes.Coding;
+import gov.va.api.health.stu3.api.datatypes.ContactPoint;
+import gov.va.api.health.stu3.api.resources.Organization;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder
+final class Stu3OrganizationTransformer {
+
+  @NonNull private final DatamartOrganization datamart;
+
+  static List<Organization.OrganizationAddress> address(DatamartOrganization.Address address) {
+    if (address == null
+        || allBlank(
+            address.line1(),
+            address.line2(),
+            address.city(),
+            address.state(),
+            address.postalCode())) {
+      return null;
+    }
+    return asList(
+        Organization.OrganizationAddress.builder()
+            .text(
+                trimToNull(
+                    trimToEmpty(address.line1())
+                        + " "
+                        + trimToEmpty(address.city())
+                        + " "
+                        + trimToEmpty(address.state())
+                        + " "
+                        + trimToEmpty(address.postalCode())
+                        + " "))
+            .line(asList(address.line1(), address.line2()))
+            .city(address.city())
+            .state(address.state())
+            .postalCode(address.postalCode())
+            .build());
+  }
+
+  static Organization.OrganizationIdentifier identifier(Optional<String> npi) {
+    if (isBlank(npi)) {
+      return null;
+    }
+    return Organization.OrganizationIdentifier.builder()
+        .system("http://hl7.org/fhir/sid/us-npi")
+        .value(npi.get())
+        .build();
+  }
+
+  static ContactPoint telecom(DatamartOrganization.Telecom telecom) {
+    if (telecom == null || allBlank(telecom.system(), telecom.value())) {
+      return null;
+    }
+    return convert(
+        telecom,
+        tel ->
+            ContactPoint.builder().system(telecomSystem(tel.system())).value(tel.value()).build());
+  }
+
+  static ContactPoint.ContactPointSystem telecomSystem(DatamartOrganization.Telecom.System tel) {
+    return convert(
+        tel, source -> EnumSearcher.of(ContactPoint.ContactPointSystem.class).find(tel.toString()));
+  }
+
+  static List<CodeableConcept> type(Optional<DatamartCoding> type) {
+    if (type.isEmpty()) {
+      return null;
+    }
+    return List.of(CodeableConcept.builder()
+            .coding(asList(
+                    Coding.builder()
+                          .display(type.get().code().toString()).build()))
+            .text(type.get().display().toString())
+            .build());
+  }
+
+  List<ContactPoint> telecoms() {
+    return emptyToNull(
+        datamart.telecom().stream().map(tel -> telecom(tel)).collect(Collectors.toList()));
+  }
+
+  public Organization toFhir() {
+    return Organization.builder()
+        .resourceType("Organization")
+        .id(datamart.cdwId())
+        .identifier(asList(identifier(datamart.npi())))
+        .active(datamart.active())
+        .type(type(datamart.type()))
+        .name(datamart.name())
+        .telecom(telecoms())
+        .address(address(datamart.address()))
+        .build();
+  }
+}
