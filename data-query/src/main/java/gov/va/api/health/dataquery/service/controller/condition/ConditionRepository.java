f2dce5cae3af208fa70f4311e19be58817d3d763
Evan Clendenning <evan.clendenning@libertyits.com>
2020-02-11 13:42:47 -0500

    add read uncommitted to repositories (#474)

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/condition/ConditionRepository.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/condition/ConditionRepository.java
@@ -5,8 +5,11 @@ import java.util.Set;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Loggable
+@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 public interface ConditionRepository extends PagingAndSortingRepository<ConditionEntity, String> {
   Page<ConditionEntity> findByIcn(String icn, Pageable pageable);
 
--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/location/LocationRepository.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/location/LocationRepository.java
@@ -14,8 +14,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Loggable
+@Transactional(isolation = Isolation.READ_UNCOMMITTED)
 public interface LocationRepository
     extends PagingAndSortingRepository<LocationEntity, String>,
         JpaSpecificationExecutor<LocationEntity> {
