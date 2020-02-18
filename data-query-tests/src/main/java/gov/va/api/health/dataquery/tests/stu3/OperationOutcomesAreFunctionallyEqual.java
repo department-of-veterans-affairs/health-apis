c8e1cfdfad432c0377c6ff30a5171d1958759466
Peter Branam-Lefkove <peter.branam-lefkove@libertyits.com>
2020-02-18 13:53:16 -0500

    .

--- data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/stu3/OperationOutcomesAreFunctionallyEqual.java
+++ data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/stu3/OperationOutcomesAreFunctionallyEqual.java
@@ -3,31 +3,22 @@ package gov.va.api.health.dataquery.tests.stu3;
 import gov.va.api.health.sentinel.ErrorsAreFunctionallyEqual;
 import gov.va.api.health.stu3.api.resources.OperationOutcome;
 import io.restassured.response.ResponseBody;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 
-/**
- * Compares two OperationalOutcomes present in a response after first removing the transient time
- * stamp data.
- */
 @Slf4j
 public final class OperationOutcomesAreFunctionallyEqual implements ErrorsAreFunctionallyEqual {
-  /**
-   * Remove data from the OO that is unique for each instance. This includes the generated ID and
-   * the timestamp.
-   */
+  /** Remove fields unique to each instance: generated ID, timestamp, and encrypted data. */
   private OperationOutcome asOperationOutcomeWithoutDiagnostics(ResponseBody<?> body) {
     try {
       OperationOutcome oo = body.as(OperationOutcome.class);
       oo.id("REMOVED-FOR-COMPARISON");
-      oo.issue()
-          .forEach(
-              i -> {
-                if (i.diagnostics() != null) {
-                  i.diagnostics(
-                      i.diagnostics()
-                          .replaceAll("Timestamp:.*(\n|$)", "Timestamp:REMOVED-FOR-COMPARISON"));
-                }
-              });
+      oo.extension().stream()
+          .filter(e -> e.url().equals("timestamp"))
+          .forEach(e -> e.valueInstant("REMOVED-FOR-COMPARISON"));
+      oo.extension().stream()
+          .filter(e -> List.of("message", "cause", "request").contains(e.url()))
+          .forEach(e -> e.valueString("REMOVED-FOR-COMPARISON"));
       return oo;
     } catch (Exception e) {
       log.error("Failed read response as OperationOutcome: {}", body.prettyPrint());
@@ -36,9 +27,9 @@ public final class OperationOutcomesAreFunctionallyEqual implements ErrorsAreFun
   }
 
   @Override
-  public boolean equals(ResponseBody<?> responseBody1, ResponseBody<?> responseBody2) {
-    OperationOutcome oo1 = asOperationOutcomeWithoutDiagnostics(responseBody1);
-    OperationOutcome oo2 = asOperationOutcomeWithoutDiagnostics(responseBody2);
-    return oo1.equals(oo2);
+  public boolean equals(ResponseBody<?> left, ResponseBody<?> right) {
+    OperationOutcome ooLeft = asOperationOutcomeWithoutDiagnostics(left);
+    OperationOutcome ooRight = asOperationOutcomeWithoutDiagnostics(right);
+    return ooLeft.equals(ooRight);
   }
 }
