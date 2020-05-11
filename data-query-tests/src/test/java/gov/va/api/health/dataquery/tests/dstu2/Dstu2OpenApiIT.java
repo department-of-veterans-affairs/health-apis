79da89b3dc12c372aad7b013210897785f538832
jhulbert <joshua.hulbert@libertyits.com>
2020-05-11 12:26:51 -0400

    Integration Test Update (Removes Yaml Check)

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/dstu2/Dstu2OpenApiIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/dstu2/Dstu2OpenApiIT.java
@@ -8,7 +8,6 @@ import gov.va.api.health.dataquery.tests.categories.LabDataQueryPatient;
 import gov.va.api.health.dataquery.tests.categories.ProdDataQueryPatient;
 import gov.va.api.health.sentinel.categories.Local;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -24,8 +23,11 @@ public class Dstu2OpenApiIT {
   @Category({Local.class, LabDataQueryPatient.class, ProdDataQueryPatient.class})
   public void openApiIsValid() {
     log.info("Verify {}openapi.json is valid (200)", apiPath());
-    assertThat(RestAssured.given()
-            .spec(ResourceVerifier.dstu2().dataQuery().service().requestSpecification())
-            .get(apiPath() + "openapi.json").statusCode()).isEqualTo(200);
+    assertThat(
+            RestAssured.given()
+                .spec(ResourceVerifier.dstu2().dataQuery().service().requestSpecification())
+                .get(apiPath() + "openapi.json")
+                .statusCode())
+        .isEqualTo(200);
   }
 }
