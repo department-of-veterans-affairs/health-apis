18e58a625245c27c7a086371e2d420bfeb8bdc6a
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2020-05-19 14:38:53 -0400

    Removed null checks on optional items.

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medication/R4MedicationTransformer.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/medication/R4MedicationTransformer.java
@@ -36,7 +36,7 @@ public class R4MedicationTransformer {
    * the local drug name as text.
    */
   CodeableConcept bestCode() {
-    if (datamart.rxnorm() != null && datamart.rxnorm().isPresent()) {
+    if (!datamart.rxnorm().isEmpty()) {
       DatamartMedication.RxNorm rxNorm = datamart.rxnorm().get();
       return CodeableConcept.builder()
           .coding(
@@ -49,7 +49,7 @@ public class R4MedicationTransformer {
           .text(rxNorm.text())
           .build();
     }
-    if (datamart.product() != null && datamart.product().isPresent()) {
+    if (!datamart.product().isEmpty()) {
       DatamartMedication.Product product = datamart.product().get();
       return CodeableConcept.builder()
           .coding(
