50fbe8ed74a5471d05f229f1814d59ccb91b01da
ilaflamme-va <43884028+ilaflamme-va@users.noreply.github.com>
2020-05-29 13:05:49 -0400

    uscorer4 procedure transformer (#517)
    * uscorer4 procedure transformer

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/procedure/ProcedureSamples.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/procedure/ProcedureSamples.java
@@ -6,14 +6,6 @@ import gov.va.api.health.argonaut.api.resources.Procedure.Entry;
 import gov.va.api.health.dataquery.service.controller.datamart.DatamartCoding;
 import gov.va.api.health.dataquery.service.controller.datamart.DatamartReference;
 import gov.va.api.health.dataquery.service.controller.procedure.DatamartProcedure.Status;
-import gov.va.api.health.dstu2.api.bundle.AbstractBundle.BundleType;
-import gov.va.api.health.dstu2.api.bundle.AbstractEntry.Search;
-import gov.va.api.health.dstu2.api.bundle.AbstractEntry.SearchMode;
-import gov.va.api.health.dstu2.api.bundle.BundleLink;
-import gov.va.api.health.dstu2.api.bundle.BundleLink.LinkRelation;
-import gov.va.api.health.dstu2.api.datatypes.CodeableConcept;
-import gov.va.api.health.dstu2.api.datatypes.Coding;
-import gov.va.api.health.dstu2.api.elements.Reference;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collection;
@@ -65,10 +57,13 @@ public class ProcedureSamples {
   @AllArgsConstructor(staticName = "create")
   public static class Dstu2 {
     static Bundle asBundle(
-        String baseUrl, Collection<Procedure> resources, int totalRecords, BundleLink... links) {
+        String baseUrl,
+        Collection<Procedure> resources,
+        int totalRecords,
+        gov.va.api.health.dstu2.api.bundle.BundleLink... links) {
       return Bundle.builder()
           .resourceType("Bundle")
-          .type(BundleType.searchset)
+          .type(gov.va.api.health.dstu2.api.bundle.AbstractBundle.BundleType.searchset)
           .total(totalRecords)
           .link(Arrays.asList(links))
           .entry(
@@ -78,14 +73,23 @@ public class ProcedureSamples {
                           Entry.builder()
                               .fullUrl(baseUrl + "/Procedure/" + c.id())
                               .resource(c)
-                              .search(Search.builder().mode(SearchMode.match).build())
+                              .search(
+                                  gov.va.api.health.dstu2.api.bundle.AbstractEntry.Search.builder()
+                                      .mode(
+                                          gov.va.api.health.dstu2.api.bundle.AbstractEntry
+                                              .SearchMode.match)
+                                      .build())
                               .build())
                   .collect(Collectors.toList()))
           .build();
     }
 
-    static BundleLink link(LinkRelation rel, String base, int page, int count) {
-      return BundleLink.builder()
+    static gov.va.api.health.dstu2.api.bundle.BundleLink link(
+        gov.va.api.health.dstu2.api.bundle.BundleLink.LinkRelation rel,
+        String base,
+        int page,
+        int count) {
+      return gov.va.api.health.dstu2.api.bundle.BundleLink.builder()
           .relation(rel)
           .url(base + "&page=" + page + "&_count=" + count)
           .build();
@@ -102,10 +106,10 @@ public class ProcedureSamples {
           .subject(reference("VETERAN,GRAY PRO", "Patient/" + patientId))
           .status(Procedure.Status.completed)
           .code(
-              CodeableConcept.builder()
+              gov.va.api.health.dstu2.api.datatypes.CodeableConcept.builder()
                   .coding(
                       List.of(
-                          Coding.builder()
+                          gov.va.api.health.dstu2.api.datatypes.Coding.builder()
                               .code("90870")
                               .system("http://www.ama-assn.org/go/cpt")
                               .display("ELECTROCONVULSIVE THERAPY")
@@ -113,14 +117,60 @@ public class ProcedureSamples {
                   .build())
           .notPerformed(true)
           .reasonNotPerformed(
-              List.of(CodeableConcept.builder().text("CASE MOVED TO EARLIER DATE").build()))
+              List.of(
+                  gov.va.api.health.dstu2.api.datatypes.CodeableConcept.builder()
+                      .text("CASE MOVED TO EARLIER DATE")
+                      .build()))
           .performedDateTime(performedOn)
           .location(reference("ZZPSYCHIATRY", "Location/237281"))
           .build();
     }
 
-    Reference reference(String display, String ref) {
-      return Reference.builder().display(display).reference(ref).build();
+    gov.va.api.health.dstu2.api.elements.Reference reference(String display, String ref) {
+      return gov.va.api.health.dstu2.api.elements.Reference.builder()
+          .display(display)
+          .reference(ref)
+          .build();
+    }
+  }
+
+  @AllArgsConstructor(staticName = "create")
+  public static class R4 {
+    gov.va.api.health.uscorer4.api.resources.Procedure procedure() {
+      return procedure("1000000719261", "1004476237V111282", "2008-01-02T06:00:00Z");
+    }
+
+    gov.va.api.health.uscorer4.api.resources.Procedure procedure(
+        String id, String patientId, String performedOn) {
+      return gov.va.api.health.uscorer4.api.resources.Procedure.builder()
+          .resourceType("Procedure")
+          .id(id)
+          .subject(reference("VETERAN,GRAY PRO", "Patient/" + patientId))
+          .status(gov.va.api.health.uscorer4.api.resources.Procedure.Status.completed)
+          .code(
+              gov.va.api.health.r4.api.datatypes.CodeableConcept.builder()
+                  .coding(
+                      List.of(
+                          gov.va.api.health.r4.api.datatypes.Coding.builder()
+                              .code("90870")
+                              .system("http://www.ama-assn.org/go/cpt")
+                              .display("ELECTROCONVULSIVE THERAPY")
+                              .build()))
+                  .build())
+          .statusReason(
+              gov.va.api.health.r4.api.datatypes.CodeableConcept.builder()
+                  .text("CASE MOVED TO EARLIER DATE")
+                  .build())
+          .performedDateTime(performedOn)
+          .location(reference("ZZPSYCHIATRY", "Location/237281"))
+          .build();
+    }
+
+    gov.va.api.health.r4.api.elements.Reference reference(String display, String ref) {
+      return gov.va.api.health.r4.api.elements.Reference.builder()
+          .display(display)
+          .reference(ref)
+          .build();
     }
   }
 }
