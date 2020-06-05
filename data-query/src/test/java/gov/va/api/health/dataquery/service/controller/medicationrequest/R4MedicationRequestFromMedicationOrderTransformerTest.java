c69fdd6a3c739121161cea8d516238a844d613ee
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2020-06-05 13:28:34 -0400

    Formatting

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationrequest/R4MedicationRequestFromMedicationOrderTransformerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationrequest/R4MedicationRequestFromMedicationOrderTransformerTest.java
@@ -1,15 +1,5 @@
 package gov.va.api.health.dataquery.service.controller.medicationrequest;
 
-import gov.va.api.health.dataquery.service.controller.medication.DatamartMedication;
-import gov.va.api.health.dataquery.service.controller.medication.MedicationSamples;
-import gov.va.api.health.dataquery.service.controller.medication.R4MedicationTransformer;
-import gov.va.api.health.dataquery.service.controller.medicationorder.DatamartMedicationOrder;
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class R4MedicationRequestFromMedicationOrderTransformerTest {
 
 
-
-}
+public class R4MedicationRequestFromMedicationOrderTransformerTest {}
--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationrequest/R4MedicationRequestFromMedicationStatementTransformerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationrequest/R4MedicationRequestFromMedicationStatementTransformerTest.java
@@ -1,15 +1,5 @@
 package gov.va.api.health.dataquery.service.controller.medicationrequest;
 
-import gov.va.api.health.dataquery.service.controller.medication.DatamartMedication;
-import gov.va.api.health.dataquery.service.controller.medication.MedicationSamples;
-import gov.va.api.health.dataquery.service.controller.medicationorder.DatamartMedicationOrder;
-import gov.va.api.health.dataquery.service.controller.medicationstatement.DatamartMedicationStatement;
-import org.junit.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-public class R4MedicationRequestFromMedicationStatementTransformerTest {
 
 
-    
-}
+public class R4MedicationRequestFromMedicationStatementTransformerTest {}
