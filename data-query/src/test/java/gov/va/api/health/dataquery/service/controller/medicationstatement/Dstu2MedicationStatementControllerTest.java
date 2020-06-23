a98c42df2c4ace484a64b1608cf7f53dd1de43a2
jhulbert <joshua.hulbert@libertyits.com>
2020-06-23 13:34:27 -0400

    MedicationStatement

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationstatement/Dstu2MedicationStatementControllerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationstatement/Dstu2MedicationStatementControllerTest.java
@@ -1,6 +1,7 @@
 package gov.va.api.health.dataquery.service.controller.medicationstatement;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,15 +27,19 @@ import java.util.List;
 import javax.servlet.http.HttpServletResponse;
 import lombok.SneakyThrows;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @DataJpaTest
-@RunWith(SpringRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@ExtendWith(SpringExtension.class)
 public class Dstu2MedicationStatementControllerTest {
   HttpServletResponse response;
 
@@ -139,26 +144,26 @@ public class Dstu2MedicationStatementControllerTest {
     verify(response).addHeader("X-VA-INCLUDES-ICN", entity.icn());
   }
 
-  @Test(expected = ResourceExceptions.NotFound.class)
+  @Test
   public void readRawThrowsNotFoundWhenDataIsMissing() {
     mockMedicationStatementIdentity("1", "1");
-    controller().readRaw("1", response);
+    assertThrows(ResourceExceptions.NotFound.class, () -> controller().readRaw("1", response));
   }
 
-  @Test(expected = ResourceExceptions.NotFound.class)
+  @Test
   public void readRawThrowsNotFoundWhenIdIsUnknown() {
-    controller().readRaw("1", response);
+    assertThrows(ResourceExceptions.NotFound.class, () -> controller().readRaw("1", response));
   }
 
-  @Test(expected = ResourceExceptions.NotFound.class)
+  @Test
   public void readThrowsNotFoundWhenDataIsMissing() {
     mockMedicationStatementIdentity("1", "1");
-    controller().read("1");
+    assertThrows(ResourceExceptions.NotFound.class, () -> controller().read("1"));
   }
 
-  @Test(expected = ResourceExceptions.NotFound.class)
+  @Test
   public void readThrowsNotFoundWhenIdIsUnknown() {
-    controller().read("1");
+    assertThrows(ResourceExceptions.NotFound.class, () -> controller().read("1"));
   }
 
   @Test
--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationstatement/Dstu2MedicationStatementTransformerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/medicationstatement/Dstu2MedicationStatementTransformerTest.java
@@ -12,7 +12,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class Dstu2MedicationStatementTransformerTest {
   @Test
