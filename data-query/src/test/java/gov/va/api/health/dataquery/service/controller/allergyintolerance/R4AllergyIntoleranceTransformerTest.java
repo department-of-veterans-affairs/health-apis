d9de26b4fb0df6cf39a337f16d23bc75fcde4740
jhulbert <joshua.hulbert@libertyits.com>
2020-06-23 15:07:41 -0400

    AllergyIntolerance

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/allergyintolerance/R4AllergyIntoleranceTransformerTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/allergyintolerance/R4AllergyIntoleranceTransformerTest.java
@@ -2,7 +2,7 @@ package gov.va.api.health.dataquery.service.controller.allergyintolerance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class R4AllergyIntoleranceTransformerTest {
   @Test
