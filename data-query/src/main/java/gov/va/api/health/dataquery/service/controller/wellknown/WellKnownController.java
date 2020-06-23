755faa5e4a302e2c3c92149b42089b0ecf92b6f6
Larry Crochet <larry.crochet@libertyits.com>
2020-06-23 15:55:38 -0400

    manage and revoke endpoints in extention

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/wellknown/WellKnownController.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/wellknown/WellKnownController.java
@@ -22,6 +22,8 @@ class WellKnownController {
     return WellKnown.builder()
         .authorizationEndpoint(metadataProperties.getSecurity().getAuthorizeEndpoint())
         .tokenEndpoint(metadataProperties.getSecurity().getTokenEndpoint())
+        .managementEndpoint(metadataProperties.getSecurity().getManageEndpoint())
+        .revocationEndpoint(metadataProperties.getSecurity().getRevocationEndpoint())
         .capabilities(wellKnownProperties.getCapabilities())
         .responseTypeSupported(wellKnownProperties.getResponseTypeSupported())
         .scopesSupported(wellKnownProperties.getScopesSupported())
