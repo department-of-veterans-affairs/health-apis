74c8800f0894135964cf71bc07b0ce2b563007a5
taylorrassmannlibertyits <64798603+taylorrassmannlibertyits@users.noreply.github.com>
2020-06-26 14:48:10 -0400

    Added assume local to HealthIT.

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/HealthCheckIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/HealthCheckIT.java
@@ -1,5 +1,6 @@
 package gov.va.api.health.dataquery.tests;
 
+import static gov.va.api.health.dataquery.tests.TestAssumptionUtility.assumeLocal;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 public class HealthCheckIT {
   @Test
   public void dataQueryIsHealthy() {
+    assumeLocal();
     TestClients.dstu2DataQuery()
         .get("/actuator/health")
         .response()
