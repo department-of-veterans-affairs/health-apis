f77bca815fb5a28da97c30476371568da16a1fce
eclendenning-va <evan.clendenning@libertyits.com>
2020-07-09 10:43:40 -0400

    code token based searching

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/ObservationIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/ObservationIT.java
@@ -61,9 +61,8 @@ public class ObservationIT {
         test(
             200,
             Observation.Bundle.class,
-            "Observation?patient={patient}&code={loinc1}",
-            verifier.ids().patient(),
-            verifier.ids().observations().loinc1()),
+            "Observation?patient={patient}&code=http://loinc.org|",
+            verifier.ids().patient()),
         test(
             200,
             Observation.Bundle.class,
@@ -82,7 +81,7 @@ public class ObservationIT {
         test(
             200,
             Observation.Bundle.class,
-            "Observation?patient={patient}&code={loinc1},{loinc2}",
+            "Observation?patient={patient}&code=http://loinc.org|{loinc1},{loinc2}",
             verifier.ids().patient(),
             verifier.ids().observations().loinc1(),
             verifier.ids().observations().loinc2()),
