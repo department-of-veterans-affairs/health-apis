a3714095cae56ce5e2598336b92bab2ab3c484eb
jhulbert <joshua.hulbert@libertyits.com>
2020-07-23 14:25:06 -0400

    Category search by set

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/diagnosticreport/DiagnosticReportRepository.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/diagnosticreport/DiagnosticReportRepository.java
@@ -3,7 +3,9 @@ package gov.va.api.health.dataquery.service.controller.diagnosticreport;
 import gov.va.api.health.autoconfig.logging.Loggable;
 import gov.va.api.health.dataquery.service.controller.DateTimeParameters;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
@@ -29,7 +31,7 @@ public interface DiagnosticReportRepository
   class PatientAndCategoryAndDateSpecification implements Specification<DiagnosticReportEntity> {
     String patient;
 
-    String category;
+    Set<String> categories;
 
     DateTimeParameters date1;
 
@@ -37,9 +39,9 @@ public interface DiagnosticReportRepository
 
     @Builder
     private PatientAndCategoryAndDateSpecification(
-        String patient, String category, String[] dates) {
+        String patient, Set<String> categories, String[] dates) {
       this.patient = patient;
-      this.category = category;
+      this.categories = new HashSet<>(categories);
       date1 = (dates == null || dates.length < 1) ? null : new DateTimeParameters(dates[0]);
       date2 = (dates == null || dates.length < 2) ? null : new DateTimeParameters(dates[1]);
     }
@@ -55,7 +57,9 @@ public interface DiagnosticReportRepository
       predicates.add(criteriaBuilder.equal(root.get("icn"), patient()));
 
       // Category
-      predicates.add(criteriaBuilder.equal(root.get("category"), category()));
+      CriteriaBuilder.In<String> categoriesInClause = criteriaBuilder.in(root.get("category"));
+      categories.forEach(categoriesInClause::value);
+      predicates.add(categoriesInClause);
 
       // Date(s)
       if (date1() != null) {
