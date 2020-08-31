b4a865b85c07e0806819f1f27a3f7df9d04f2d0d
Ian LaFlamme <ian.laflamme@libertyits.com>
2020-08-31 14:13:40 -0400

    unit tests

--- data-query/src/test/java/gov/va/api/health/dataquery/service/controller/organization/OrganizationRepositoryTest.java
+++ data-query/src/test/java/gov/va/api/health/dataquery/service/controller/organization/OrganizationRepositoryTest.java
@@ -3,6 +3,7 @@ package gov.va.api.health.dataquery.service.controller.organization;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,64 +13,52 @@ public class OrganizationRepositoryTest {
 
   @Autowired OrganizationRepository repository;
 
-  private static final OrganizationEntity ENTITY_ONE =
-      OrganizationEntity.builder()
-          .cdwId("123")
-          .npi("npi")
-          .name("First")
-          .street("123 First Street")
-          .city("FirstCity")
-          .state("FirstState")
-          .postalCode("11111")
-          .payload("First Payload")
-          .build();
-
-  private static final OrganizationEntity ENTITY_TWO =
-      OrganizationEntity.builder()
-          .cdwId("123")
-          .npi("npi")
-          .name("Second")
-          .street("456 First Street")
-          .city("SecondCity")
-          .state("SecondState")
-          .postalCode("22222")
-          .payload("Second Payload")
-          .build();
-
-  private static final OrganizationEntity ENTITY_THREE =
-      OrganizationEntity.builder()
-          .cdwId("789")
-          .npi("npi")
-          .name("Third")
-          .street("789 Third Street")
-          .city("FirstCity")
-          .state("SecondState")
-          .postalCode("22222")
-          .payload("Third Payload")
-          .build();
-
   @Test
   void addressSpecifications() {
-
     initializeData();
     searchByAddressContainingState();
-    /*    searchByAddressContainingPostalCodeAndByAddressCity();
+    searchByAddressContainingPostalCodeAndByAddressCity();
     searchByAddressContainingStreet();
     searchByAddressContainingStateAndByAddressPostalCode();
-    searchByAddressContainingCityAndByAddressStateAndByAddressStreet();*/
+    searchByAddressContainingCityAndByAddressStateAndByAddressPostalCode();
   }
 
   private void initializeData() {
-    repository.save(ENTITY_ONE);
-    repository.save(ENTITY_TWO);
-    repository.save(ENTITY_THREE);
+    repository.save(Samples.ENTITY_ONE);
+    repository.save(Samples.ENTITY_TWO);
+    repository.save(Samples.ENTITY_THREE);
   }
 
-  private void searchByAddressContainingCityAndByAddressStateAndByAddressStreet() {}
+  private void searchByAddressContainingCityAndByAddressStateAndByAddressPostalCode() {
+    assertThat(
+        repository.findAll(
+            OrganizationRepository.AddressSpecification.builder()
+                .street("Second City")
+                .state("SecondState")
+                .postalCode("22222")
+                .build()))
+        .isEqualTo(List.of(Samples.ENTITY_TWO);
+  }
 
-  private void searchByAddressContainingStateAndByAddressPostalCode() {}
+  private void searchByAddressContainingStateAndByAddressPostalCode() {
+    assertThat(
+        repository.findAll(
+            OrganizationRepository.AddressSpecification.builder()
+                .street("FirstState")
+                .postalCode("22222")
+                .build()))
+        .isEqualTo(Lists.emptyList());
+  }
 
-  private void searchByAddressContainingPostalCodeAndByAddressCity() {}
+  private void searchByAddressContainingPostalCodeAndByAddressCity() {
+    assertThat(
+        repository.findAll(
+            OrganizationRepository.AddressSpecification.builder()
+                .street("22222")
+                .city("FirstCity")
+                .build()))
+        .isEqualTo(List.of(Samples.ENTITY_THREE));
+  }
 
   private void searchByAddressContainingState() {
     assertThat(
@@ -77,8 +66,53 @@ public class OrganizationRepositoryTest {
                 OrganizationRepository.AddressSpecification.builder()
                     .street("SecondState")
                     .build()))
-        .isEqualTo(List.of(ENTITY_TWO, ENTITY_THREE));
+        .isEqualTo(List.of(Samples.ENTITY_TWO, Samples.ENTITY_THREE));
   }
 
-  private void searchByAddressContainingStreet() {}
+  private void searchByAddressContainingStreet() {
+    assertThat(
+        repository.findAll(
+            OrganizationRepository.AddressSpecification.builder()
+                .street("123 First Street")
+                .build()))
+        .isEqualTo(List.of(Samples.ENTITY_ONE);
+  }
+
+  static class Samples {
+    private static final OrganizationEntity ENTITY_ONE =
+        OrganizationEntity.builder()
+            .cdwId("123")
+            .npi("npi")
+            .name("First")
+            .street("123 First Street")
+            .city("FirstCity")
+            .state("FirstState")
+            .postalCode("11111")
+            .payload("First Payload")
+            .build();
+
+    private static final OrganizationEntity ENTITY_TWO =
+        OrganizationEntity.builder()
+            .cdwId("123")
+            .npi("npi")
+            .name("Second")
+            .street("456 First Street")
+            .city("SecondCity")
+            .state("SecondState")
+            .postalCode("22222")
+            .payload("Second Payload")
+            .build();
+
+    private static final OrganizationEntity ENTITY_THREE =
+        OrganizationEntity.builder()
+            .cdwId("789")
+            .npi("npi")
+            .name("Third")
+            .street("789 Third Street")
+            .city("FirstCity")
+            .state("SecondState")
+            .postalCode("22222")
+            .payload("Third Payload")
+            .build();
+  }
 }
