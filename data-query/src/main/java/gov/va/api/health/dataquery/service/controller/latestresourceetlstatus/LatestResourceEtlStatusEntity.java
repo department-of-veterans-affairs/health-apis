b1a29f478e447ce92f406d8da4741f62aaa8580f
Adrian Garcia <adrian.garcia@libertyits.com>
2020-09-21 13:15:33 -0400

    Added new entity for Latest Resource ETL Status Table

---
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/latestresourceetlstatus/LatestResourceEtlStatusEntity.java
@@ -0,0 +1,36 @@
+package gov.va.api.health.dataquery.service.controller.latestresourceetlstatus;
+
+import java.time.Instant;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Sort;
+
+@Data
+@Entity
+@Builder
+@Table(name = "Latest_Resource_ETL_Status", schema = "app")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class LatestResourceEtlStatusEntity {
+
+  @Id
+  @Column(name = "ResourceName")
+  @EqualsAndHashCode.Include
+  private String resourceName;
+
+  @Column(name = "EndDateTimeUTC")
+  private Instant endDateTime;
+
+  static Sort naturalOrder() {
+    return Sort.by("ResourceName").ascending();
+  }
+}
