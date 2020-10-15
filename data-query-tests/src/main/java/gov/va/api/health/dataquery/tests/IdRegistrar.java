1d83bd00245db04d37de9d89390d40e7ab2c530d
Adrian Garcia <adrian.garcia@libertyits.com>
2020-10-15 12:06:00 -0400

    Removed unnecessary parts of registeredCdwIds

--- data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/IdRegistrar.java
+++ data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/IdRegistrar.java
@@ -1,9 +1,6 @@
 package gov.va.api.health.dataquery.tests;
 
-import gov.va.api.health.ids.api.ResourceIdentity;
 import gov.va.api.health.sentinel.ReducedSpamLogger;
-import java.util.Arrays;
-import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Value;
@@ -21,47 +18,12 @@ public final class IdRegistrar {
   @Getter(lazy = true)
   TestIds registeredIds = registerCdwIds();
 
-  private ResourceIdentity id(String type, String id) {
-    return ResourceIdentity.builder().system("CDW").resource(type).identifier(id).build();
-  }
-
   private TestIds registerCdwIds() {
     TestIds cdwIds = system().cdwIds();
     if (cdwIds.publicIds()) {
       log.infoOnce("Registration not necessary");
       return cdwIds;
     }
-
-    ResourceIdentity allergyIntolerance = id("ALLERGY_INTOLERANCE", cdwIds.allergyIntolerance());
-    ResourceIdentity condition = id("CONDITION", cdwIds.condition());
-    ResourceIdentity diagnosticReport = id("DIAGNOSTIC_REPORT", cdwIds.diagnosticReport());
-    ResourceIdentity immunization = id("IMMUNIZATION", cdwIds.immunization());
-    ResourceIdentity location = id("LOCATION", cdwIds.location());
-    ResourceIdentity medication = id("MEDICATION", cdwIds.medication());
-    ResourceIdentity medicationOrder = id("MEDICATION_ORDER", cdwIds.medicationOrder());
-    ResourceIdentity medicationStatement = id("MEDICATION_STATEMENT", cdwIds.medicationStatement());
-    ResourceIdentity observation = id("OBSERVATION", cdwIds.observation());
-    ResourceIdentity organizataion = id("ORGANIZATION", cdwIds.organization());
-    ResourceIdentity patient = id("PATIENT", cdwIds.patient());
-    ResourceIdentity practitioner = id("PRACTITIONER", cdwIds.practitioner());
-    ResourceIdentity procedure = id("PROCEDURE", cdwIds.procedure());
-
-    List<ResourceIdentity> identities =
-        Arrays.asList(
-            allergyIntolerance,
-            condition,
-            diagnosticReport,
-            immunization,
-            location,
-            patient,
-            medication,
-            medicationOrder,
-            medicationStatement,
-            observation,
-            organizataion,
-            practitioner,
-            procedure);
-    log.info("Registering {}", identities);
     return null;
   }
 }
