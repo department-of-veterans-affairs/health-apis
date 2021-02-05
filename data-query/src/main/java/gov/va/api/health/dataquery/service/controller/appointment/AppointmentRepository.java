e3ab8d5384502bcc48d818fef0bd2d56ad724539
Ian LaFlamme <ian.laflamme@libertyits.com>
2021-02-05 15:21:27 -0500

    .

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/appointment/AppointmentRepository.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/appointment/AppointmentRepository.java
@@ -2,14 +2,6 @@ package gov.va.api.health.dataquery.service.controller.appointment;
 
 import gov.va.api.health.autoconfig.logging.Loggable;
 import gov.va.api.lighthouse.datamart.CompositeCdwId;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-import lombok.RequiredArgsConstructor;
-import lombok.Value;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.transaction.annotation.Isolation;
@@ -19,18 +11,4 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(isolation = Isolation.READ_UNCOMMITTED)
 public interface AppointmentRepository
     extends PagingAndSortingRepository<AppointmentEntity, CompositeCdwId>,
-        JpaSpecificationExecutor<AppointmentEntity> {
-  @Value
-  @RequiredArgsConstructor(staticName = "of")
-  class PatientSpecification implements Specification<AppointmentEntity> {
-    String icn;
-
-    @Override
-    public Predicate toPredicate(
-        Root<AppointmentEntity> root,
-        CriteriaQuery<?> criteriaQuery,
-        CriteriaBuilder criteriaBuilder) {
-      return criteriaBuilder.equal(root.get("icn"), icn());
-    }
-  }
-}
+        JpaSpecificationExecutor<AppointmentEntity> {}
