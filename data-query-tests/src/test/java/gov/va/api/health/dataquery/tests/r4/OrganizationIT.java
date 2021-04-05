68e732d4a1ac1e0a3f5aeedf8b20ba02d59661ea
Joshua Hulbert <43861345+jhulbert-va@users.noreply.github.com>
2021-04-05 09:56:33 -0400

    Vulcanize R4 Organization (#690)

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/OrganizationIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/OrganizationIT.java
@@ -5,6 +5,7 @@ import gov.va.api.health.dataquery.tests.TestIds;
 import gov.va.api.health.fhir.testsupport.ResourceVerifier;
 import gov.va.api.health.r4.api.resources.OperationOutcome;
 import gov.va.api.health.r4.api.resources.Organization;
+import java.util.function.Predicate;
 import lombok.experimental.Delegate;
 import org.junit.jupiter.api.Test;
 
@@ -15,43 +16,65 @@ public class OrganizationIT {
 
   @Test
   void advanced() {
-    verifyAll( // Search by _id
-        test(200, Organization.Bundle.class, "Organization?_id={id}", testIds.organization()),
-        test(404, OperationOutcome.class, "Organization?_id={id}", testIds.unknown()),
-        test(
-            200, Organization.Bundle.class, "Organization?identifier={id}", testIds.organization()),
-        test(404, OperationOutcome.class, "Organization?identifier={id}", testIds.unknown()),
+    verifyAll(
         test(
             200,
             Organization.Bundle.class,
+            bundleIsNotEmpty(),
+            "Organization?_id={id}",
+            testIds.organization()),
+        test(
+            200,
+            Organization.Bundle.class,
+            bundleIsNotEmpty().negate(),
+            "Organization?_id={id}",
+            testIds.unknown()),
+        test(
+            200,
+            Organization.Bundle.class,
+            bundleIsNotEmpty(),
+            "Organization?identifier={id}",
+            testIds.organization()),
+        test(
+            200,
+            Organization.Bundle.class,
+            bundleIsNotEmpty(),
             "Organization?name={name}",
             testIds.organizations().name()),
         test(
             200,
             Organization.Bundle.class,
+            bundleIsNotEmpty(),
             "Organization?address={street}",
             testIds.organizations().addressStreet()),
         test(
             200,
             Organization.Bundle.class,
+            bundleIsNotEmpty(),
             "Organization?address-city={city}",
             testIds.organizations().addressCity()),
         test(
             200,
             Organization.Bundle.class,
+            bundleIsNotEmpty(),
             "Organization?address-state={state}",
             testIds.organizations().addressState()),
         test(
             200,
             Organization.Bundle.class,
+            bundleIsNotEmpty(),
             "Organization?address-postalcode={zip}",
             testIds.organizations().addressPostalCode()));
   }
 
   @Test
   void basic() {
-    verifyAll( // Read
+    verifyAll(
         test(200, Organization.class, "Organization/{id}", testIds.organization()),
         test(404, OperationOutcome.class, "Organization/{id}", testIds.unknown()));
+  }
+
+  private Predicate<Organization.Bundle> bundleIsNotEmpty() {
+    return bundle -> !bundle.entry().isEmpty();
   }
 }
