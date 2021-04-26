7160c7f0c784cd763bbf8ff223c13b63ed90a208
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2021-04-26 18:07:53 -0400

    Tests: Update oauth tests to use health-apis-parent AccessToken class (#705)

--- data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/Oauth.java
+++ data-query-tests/src/main/java/gov/va/api/health/dataquery/tests/Oauth.java
@@ -1,17 +1,11 @@
 package gov.va.api.health.dataquery.tests;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import gov.va.api.health.sentinel.ExpectedResponse;
-import gov.va.api.health.sentinel.OauthRobotProperties;
 import gov.va.api.health.sentinel.ServiceDefinition;
-import gov.va.api.health.sentinel.SystemOauthRobot;
-import gov.va.api.health.sentinel.TokenExchange;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.specification.RequestSpecification;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -19,20 +13,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @UtilityClass
 public class Oauth {
-  /** Get an access token with the provided scopes associated. */
-  public String exchangeToken(List<String> scopes) {
-    SystemOauthRobot.Configuration config =
-        OauthRobotProperties.usingSystemProperties()
-            .forSystemOauth()
-            .defaultScopes(scopes)
-            .build()
-            .systemOauthConfig();
-    TokenExchange token = SystemOauthRobot.builder().config(config).build().token();
-    if (token.isError()) {
-      fail("Failed to get token result from oauth robot.");
-    }
-    return token.accessToken();
-  }
 
   /** A rest-assured request, with a given access-token. */
   public void test(
