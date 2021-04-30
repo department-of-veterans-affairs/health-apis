1a432b4f470aa455d74b746a9d8646fb84ecfbfc
jabando-va <jesse.abando@libertyits.com>
2021-04-30 17:46:42 -0400

    ProcedureITs check bundle emptiness

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/ProcedureIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/ProcedureIT.java
@@ -8,6 +8,7 @@ import gov.va.api.health.fhir.testsupport.ResourceVerifier;
 import gov.va.api.health.r4.api.resources.OperationOutcome;
 import gov.va.api.health.r4.api.resources.Procedure;
 import gov.va.api.health.sentinel.Environment;
+import java.util.function.Predicate;
 import lombok.experimental.Delegate;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,10 @@ public class ProcedureIT {
   @Delegate ResourceVerifier verifier = DataQueryResourceVerifier.r4();
 
   TestIds testIds = DataQueryResourceVerifier.ids();
+
+  private Predicate<Procedure.Bundle> bundleIsNotEmpty() {
+    return bundle -> !bundle.entry().isEmpty();
+  }
 
   @Test
   public void read() {
@@ -30,24 +35,36 @@ public class ProcedureIT {
         test(
             200,
             Procedure.Bundle.class,
-            b -> b.entry().isEmpty(),
+            bundleIsNotEmpty().negate(),
             "Procedure?_id={id}",
             testIds.unknown()),
-        test(200, Procedure.Bundle.class, "Procedure?identifier={id}", testIds.procedure()),
         test(
             200,
             Procedure.Bundle.class,
+            bundleIsNotEmpty(),
+            "Procedure?identifier={id}",
+            testIds.procedure()),
+        test(
+            200,
+            Procedure.Bundle.class,
+            bundleIsNotEmpty(),
             "Procedure?patient={patient}&date={onDate}",
             testIds.patient(),
             testIds.procedures().onDate()),
         test(
             200,
             Procedure.Bundle.class,
+            bundleIsNotEmpty(),
             "Procedure?patient={patient}&date={fromDate}&date={toDate}",
             testIds.patient(),
             testIds.procedures().fromDate(),
             testIds.procedures().toDate()),
-        test(200, Procedure.Bundle.class, "Procedure?patient={patient}", testIds.patient()));
+        test(
+            200,
+            Procedure.Bundle.class,
+            bundleIsNotEmpty(),
+            "Procedure?patient={patient}",
+            testIds.patient()));
   }
 
   @Test
