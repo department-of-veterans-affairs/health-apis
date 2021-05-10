fcbe23178c72f164ea639d44d9fabde88f662ba9
jhulbert <joshua.hulbert@libertyits.com>
2021-05-10 11:48:52 -0400

    Update IT checks

--- data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/MedicationRequestIT.java
+++ data-query-tests/src/test/java/gov/va/api/health/dataquery/tests/r4/MedicationRequestIT.java
@@ -8,6 +8,7 @@ import gov.va.api.health.fhir.testsupport.ResourceVerifier;
 import gov.va.api.health.r4.api.resources.MedicationRequest;
 import gov.va.api.health.r4.api.resources.OperationOutcome;
 import gov.va.api.health.sentinel.Environment;
+import java.util.function.Predicate;
 import lombok.experimental.Delegate;
 import org.junit.jupiter.api.Test;
 
@@ -15,6 +16,10 @@ public class MedicationRequestIT {
   @Delegate ResourceVerifier verifier = DataQueryResourceVerifier.r4();
 
   TestIds testIds = DataQueryResourceVerifier.ids();
+
+  private Predicate<MedicationRequest.Bundle> bundleHasResults() {
+    return bundle -> !bundle.entry().isEmpty();
+  }
 
   @Test
   public void read() {
@@ -31,44 +36,51 @@ public class MedicationRequestIT {
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?_id={id}",
             testIds.medicationOrder()),
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?_id={id}",
             testIds.medicationStatement()),
         test(
             200,
             MedicationRequest.Bundle.class,
-            b -> b.entry().isEmpty(),
+            bundleHasResults().negate(),
             "MedicationRequest?_id={id}",
             testIds.unknown()),
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?identifier={id}",
             testIds.medicationOrder()),
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?identifier={id}",
             testIds.medicationStatement()),
         // Patient And Intent
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?patient={patient}&intent=order",
             testIds.patient()),
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?patient={patient}&intent=plan",
             testIds.patient()),
         // Patient Icn
         test(
             200,
             MedicationRequest.Bundle.class,
+            bundleHasResults(),
             "MedicationRequest?patient={patient}",
             testIds.patient()));
   }
