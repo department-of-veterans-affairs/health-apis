666400770a037a54b250abf725a65ab716f6b1e3
Adrian Garcia <adrian.garcia@libertyits.com>
2021-05-18 16:24:51 -0400

    Corrected previously unnoticed merge conflict.

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/appointment/R4AppointmentTransformer.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/appointment/R4AppointmentTransformer.java
@@ -7,6 +7,7 @@ import static gov.va.api.health.dataquery.service.controller.Transformers.isBlan
 import gov.va.api.health.dataquery.service.controller.R4Transformers;
 import gov.va.api.health.r4.api.datatypes.CodeableConcept;
 import gov.va.api.health.r4.api.datatypes.Coding;
+import gov.va.api.health.r4.api.elements.Meta;
 import gov.va.api.health.r4.api.resources.Appointment;
 import gov.va.api.lighthouse.datamart.CompositeCdwId;
 import gov.va.api.lighthouse.datamart.DatamartReference;
@@ -86,6 +87,14 @@ final class R4AppointmentTransformer {
 
   boolean isWaitlist() {
     return compositeCdwId.cdwIdResourceCode() == 'W';
+  }
+
+  Meta meta(Instant dmLastUpdated) {
+    String lastUpdated = asDateTimeString(dmLastUpdated);
+    if (lastUpdated == null) {
+      return null;
+    }
+    return Meta.builder().lastUpdated(lastUpdated).build();
   }
 
   Integer minutesDuration(Optional<Integer> maybeMinutesDuration) {
@@ -256,6 +265,7 @@ final class R4AppointmentTransformer {
   Appointment toFhir() {
     return Appointment.builder()
         .id(dm.cdwId())
+        .meta(meta(dm.lastUpdated()))
         .status(status(dm.start(), dm.status(), dm.visitSid()))
         .cancelationReason(cancelationReason(dm.cancelationReason()))
         .specialty(specialty(dm.specialty()))
