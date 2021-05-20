c9112fe875404eddefa38be6c9798d88071ff39b
Adrian Garcia <adrian.garcia@libertyits.com>
2021-05-20 17:01:36 -0400

    Added service type mapping to Appointment Transformer, and expanded Appointmnet Samples accordingly

--- data-query/src/main/java/gov/va/api/health/dataquery/service/controller/appointment/R4AppointmentTransformer.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/controller/appointment/R4AppointmentTransformer.java
@@ -190,6 +190,13 @@ final class R4AppointmentTransformer {
     }
   }
 
+  List<CodeableConcept> serviceType(String serviceType) {
+    if (isBlank(serviceType)) {
+      return null;
+    }
+    return List.of(CodeableConcept.builder().text(serviceType).build());
+  }
+
   List<CodeableConcept> specialty(Optional<String> maybeSpecialty) {
     if (isBlank(maybeSpecialty)) {
       return null;
@@ -273,6 +280,7 @@ final class R4AppointmentTransformer {
         .cancelationReason(cancelationReason(dm.cancelationReason()))
         .serviceCategory(serviceCategory(dm.serviceCategory()))
         .specialty(specialty(dm.specialty()))
+        .serviceType(serviceType(dm.serviceType()))
         .appointmentType(appointmentType(dm.appointmentType()))
         .description(description(dm.description()))
         .start(asDateTimeString(dm.start()))
