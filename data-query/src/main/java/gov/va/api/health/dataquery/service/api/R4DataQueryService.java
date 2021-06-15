d59a07df326ae93481c7888b1d71e5f89c44efdc
Bryan Schofield <43883561+bschofield-va@users.noreply.github.com>
2021-06-15 15:29:01 -0400

    Fix Open API to support the production and sandbox oauth configurations (#754)

--- data-query/src/main/java/gov/va/api/health/dataquery/service/api/R4DataQueryService.java
+++ data-query/src/main/java/gov/va/api/health/dataquery/service/api/R4DataQueryService.java
@@ -18,7 +18,6 @@ import gov.va.api.health.r4.api.PractitionerRoleApi;
 import gov.va.api.health.r4.api.ProcedureApi;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.OAuthFlow;
@@ -26,31 +25,52 @@ import io.swagger.v3.oas.annotations.security.OAuthFlows;
 import io.swagger.v3.oas.annotations.security.OAuthScope;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.security.SecuritySchemes;
 import io.swagger.v3.oas.annotations.servers.Server;
 import javax.ws.rs.Path;
 
 /** R4 Swagger Definition. */
 @OpenAPIDefinition(
-    security =
-        @SecurityRequirement(
-            name = "OauthFlow",
-            scopes = {
-              "patient/AllergyIntolerance.read",
-              "system/Appointment.read",
-              "patient/Condition.read",
-              "patient/Device.read",
-              "patient/DiagnosticReport.read",
-              "patient/Immunization.read",
-              "patient/Medication.read",
-              "patient/MedicationRequest.read",
-              "patient/Observation.read",
-              "patient/Patient.read",
-              "patient/Practitioner.read",
-              "patient/PractitionerRole.read",
-              "patient/Procedure.read",
-              "offline_access",
-              "launch/patient"
-            }),
+    security = {
+      @SecurityRequirement(
+          name = "OauthFlowSandbox",
+          scopes = {
+            "patient/AllergyIntolerance.read",
+            "system/Appointment.read",
+            "patient/Condition.read",
+            "patient/Device.read",
+            "patient/DiagnosticReport.read",
+            "patient/Immunization.read",
+            "patient/Medication.read",
+            "patient/MedicationRequest.read",
+            "patient/Observation.read",
+            "patient/Patient.read",
+            "patient/Practitioner.read",
+            "patient/PractitionerRole.read",
+            "patient/Procedure.read",
+            "offline_access",
+            "launch/patient"
+          }),
+      @SecurityRequirement(
+          name = "OauthFlowProduction",
+          scopes = {
+            "patient/AllergyIntolerance.read",
+            "system/Appointment.read",
+            "patient/Condition.read",
+            "patient/Device.read",
+            "patient/DiagnosticReport.read",
+            "patient/Immunization.read",
+            "patient/Medication.read",
+            "patient/MedicationRequest.read",
+            "patient/Observation.read",
+            "patient/Patient.read",
+            "patient/Practitioner.read",
+            "patient/PractitionerRole.read",
+            "patient/Procedure.read",
+            "offline_access",
+            "launch/patient"
+          })
+    },
     info =
         @Info(
             title = "US Core R4",
@@ -76,51 +96,104 @@ import javax.ws.rs.Path;
         @ExternalDocumentation(
             description = "US Core Implementation Guide",
             url = "https://build.fhir.org/ig/HL7/US-Core-R4/index.html"))
-@SecurityScheme(
-    name = "OauthFlow",
-    type = SecuritySchemeType.OAUTH2,
-    in = SecuritySchemeIn.HEADER,
-    flows =
-        @OAuthFlows(
-            implicit =
-                @OAuthFlow(
-                    authorizationUrl = "https://sandbox-api.va.gov/oauth2/authorization",
-                    tokenUrl = "https://sandbox-api.va.gov/services/fhir/v0/r4/token",
-                    scopes = {
-                      @OAuthScope(
-                          name = "patient/AllergyIntolerance.read",
-                          description = "read allergy intolerances"),
-                      @OAuthScope(
-                          name = "system/Appointment.read",
-                          description = "read appointments"),
-                      @OAuthScope(name = "patient/Condition.read", description = "read conditions"),
-                      @OAuthScope(name = "patient/Device.read", description = "read devices"),
-                      @OAuthScope(
-                          name = "patient/DiagnosticReport.read",
-                          description = "read diagnostic reports"),
-                      @OAuthScope(
-                          name = "patient/Immunization.read",
-                          description = "read immunizations"),
-                      @OAuthScope(
-                          name = "patient/Medication.read",
-                          description = "read medications"),
-                      @OAuthScope(
-                          name = "patient/MedicationRequest.read",
-                          description = "read medication requests"),
-                      @OAuthScope(
-                          name = "patient/Observation.read",
-                          description = "read observations"),
-                      @OAuthScope(name = "patient/Patient.read", description = "read patient"),
-                      @OAuthScope(
-                          name = "patient/Practitioner.read",
-                          description = "read practitioner"),
-                      @OAuthScope(
-                          name = "patient/PractitionerRole.read",
-                          description = "read practitioner roles"),
-                      @OAuthScope(name = "patient/Procedure.read", description = "read procedures"),
-                      @OAuthScope(name = "offline_access", description = "offline access"),
-                      @OAuthScope(name = "launch/patient", description = "patient launch"),
-                    })))
+@SecuritySchemes({
+  @SecurityScheme(
+      name = "OauthFlowSandbox",
+      type = SecuritySchemeType.OAUTH2,
+      flows =
+          @OAuthFlows(
+              authorizationCode =
+                  @OAuthFlow(
+                      authorizationUrl = "https://sandbox-api.va.gov/oauth2/authorization",
+                      tokenUrl = "https://sandbox-api.va.gov/services/fhir/v0/r4/token",
+                      scopes = {
+                        @OAuthScope(
+                            name = "patient/AllergyIntolerance.read",
+                            description = "read allergy intolerances"),
+                        @OAuthScope(
+                            name = "system/Appointment.read",
+                            description = "read appointments"),
+                        @OAuthScope(
+                            name = "patient/Condition.read",
+                            description = "read conditions"),
+                        @OAuthScope(name = "patient/Device.read", description = "read devices"),
+                        @OAuthScope(
+                            name = "patient/DiagnosticReport.read",
+                            description = "read diagnostic reports"),
+                        @OAuthScope(
+                            name = "patient/Immunization.read",
+                            description = "read immunizations"),
+                        @OAuthScope(
+                            name = "patient/Medication.read",
+                            description = "read medications"),
+                        @OAuthScope(
+                            name = "patient/MedicationRequest.read",
+                            description = "read medication requests"),
+                        @OAuthScope(
+                            name = "patient/Observation.read",
+                            description = "read observations"),
+                        @OAuthScope(name = "patient/Patient.read", description = "read patient"),
+                        @OAuthScope(
+                            name = "patient/Practitioner.read",
+                            description = "read practitioner"),
+                        @OAuthScope(
+                            name = "patient/PractitionerRole.read",
+                            description = "read practitioner roles"),
+                        @OAuthScope(
+                            name = "patient/Procedure.read",
+                            description = "read procedures"),
+                        @OAuthScope(name = "offline_access", description = "offline access"),
+                        @OAuthScope(name = "launch/patient", description = "patient launch"),
+                      }))),
+  @SecurityScheme(
+      name = "OauthFlowProduction",
+      type = SecuritySchemeType.OAUTH2,
+      flows =
+          @OAuthFlows(
+              authorizationCode =
+                  @OAuthFlow(
+                      authorizationUrl = "https://api.va.gov/oauth2/authorization",
+                      tokenUrl = "https://api.va.gov/services/fhir/v0/r4/token",
+                      scopes = {
+                        @OAuthScope(
+                            name = "patient/AllergyIntolerance.read",
+                            description = "read allergy intolerances"),
+                        @OAuthScope(
+                            name = "system/Appointment.read",
+                            description = "read appointments"),
+                        @OAuthScope(
+                            name = "patient/Condition.read",
+                            description = "read conditions"),
+                        @OAuthScope(name = "patient/Device.read", description = "read devices"),
+                        @OAuthScope(
+                            name = "patient/DiagnosticReport.read",
+                            description = "read diagnostic reports"),
+                        @OAuthScope(
+                            name = "patient/Immunization.read",
+                            description = "read immunizations"),
+                        @OAuthScope(
+                            name = "patient/Medication.read",
+                            description = "read medications"),
+                        @OAuthScope(
+                            name = "patient/MedicationRequest.read",
+                            description = "read medication requests"),
+                        @OAuthScope(
+                            name = "patient/Observation.read",
+                            description = "read observations"),
+                        @OAuthScope(name = "patient/Patient.read", description = "read patient"),
+                        @OAuthScope(
+                            name = "patient/Practitioner.read",
+                            description = "read practitioner"),
+                        @OAuthScope(
+                            name = "patient/PractitionerRole.read",
+                            description = "read practitioner roles"),
+                        @OAuthScope(
+                            name = "patient/Procedure.read",
+                            description = "read procedures"),
+                        @OAuthScope(name = "offline_access", description = "offline access"),
+                        @OAuthScope(name = "launch/patient", description = "patient launch"),
+                      })))
+})
 @Path("/")
 public interface R4DataQueryService
     extends AllergyIntoleranceApi,
